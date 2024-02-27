2203e5933d34a059c0a12d83d4169f19aec63c2c
Scvotr <stasforeva23@mail.ru>
2024-02-27 21:35:08 +0300

    add logger for socket events

---
+++ bk/utils/socket/socketEventLogger.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+
+const logMessage = (data, actionTypeName) => {
+  const now = new Date()
+  const currentDate = `${now.getDate()} ${now.getFullYear()}`
+  const currentTime = now.toLocaleTimeString()
+  return `\n Пользователь ID: ${data.id} ${data.name} ${currentDate} ${currentTime} ${actionTypeName};\n`
+}
+
+const logConnectionUserData = (socket) => {
+  socket.on('userConnect', (data) => {
+    const userData = {
+      id: data.userId,
+      name: data.userName,
+    };
+    fs.appendFile('socket_logs.txt', logMessage(userData, 'что то отправил'), (err) => {
+      if (err) {
+        console.log('logConnectionUserData', err)
+        throw err
+      }
+    });
+  })
+}
+
+const logConnection = (socket) => {
+  fs.appendFile('socket_logs.txt', logMessage(socket.decoded, 'подключился'), (err) => {
+    if (err) {
+      console.log('logConnection', err)
+      throw err
+    }
+  })
+  // Дополнительная логика при подключении пользователя
+}
+
+const logDisconnection = (socket) => {
+  fs.appendFile('socket_logs.txt', logMessage(socket.decoded, 'отключился'), (err) => {
+    if (err) {
+      console.log('logDisconnection', err)
+      throw err
+    }
+  });
+  // Дополнительная логика при отключении пользователя
+}
+
+const socketEventLogger = (io) => {
+  io.on('connection', (socket) => {
+    logConnection(socket);
+    logConnectionUserData(socket)
+    socket.on('disconnect', () => {
+      logDisconnection(socket);
+    });
+    // Другие обработчики событий и логика
+  });
+}
+
+module.exports = socketEventLogger;
