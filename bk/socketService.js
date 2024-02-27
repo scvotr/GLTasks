4feef8ba68a006c8b1235072a8a1d791631a0f24
Scvotr <stasforeva23@mail.ru>
2024-02-27 21:02:18 +0300

    add function for decode user by socket conection and push\pop from cache

--- bk/socketService.js
+++ bk/socketService.js
@@ -5,9 +5,52 @@ require('dotenv').config();
 
 let userSocketPool = [];
 
+function addUserInCache(userId) {
+  console.log('addUserInSocketCache: ', userId)
+  if(!userSocketPool.includes(userId)) {
+  // if(!userSocketPool.some(id => id === userId)) {
+    userSocketPool.push(userId)
+    console.log('Активных пользователй: ', userSocketPool)
+  }
+}
+
+function removeUserFromCache(userId) {
+  console.log('removeUserFromSocketCache', userId)
+  userSocketPool = userSocketPool.filter(id => id !== userId);
+  console.log('Активных пользователй: ', userSocketPool)
+}
+
+function authenticateUserSocket(socket, next) {
+  let token = socket.handshake.query.token;
+  let tokenInHeaders = socket.handshake.headers.authorization;
+  if (tokenInHeaders) {
+    tokenInHeaders = tokenInHeaders.slice(7, tokenInHeaders.length);
+    jwt.verify(tokenInHeaders, process.env.KEY_TOKEN, (err, decoded) => {
+      if (err) {
+        logger.errorAuth({
+          message: 'Authentication failed: invalid token',
+          token: tokenInHeaders
+        });
+        return next(new Error('Authentication error'));
+      }
+      socket.decoded = decoded;
+      logger.infoAuth({
+        message: 'Authentication successful',
+        decode: socket.decoded.name
+      });
+      return next();
+    });
+  } else {
+    logger.warn({
+      message: 'Authentication failed: token is missing'
+    });
+    return next(new Error('Authentication error'));
+  }
+}
+
 
 function setupSocket(io) {
-  io.use(authenticateSocket)
+  io.use(authenticateUserSocket)
     .on('connection', (socket) => {
       addUserInCache(socket.decoded.id)
     })
