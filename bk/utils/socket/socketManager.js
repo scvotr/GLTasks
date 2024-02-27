6812fd2592c03b01b3bcac906251b23a62066621
Scvotr <stasforeva23@mail.ru>
2024-02-27 16:49:04 +0300

    add module for managing WebSocket connections

---
+++ bk/utils/socket/socketManager.js
@@ -0,0 +1,19 @@
+let io = null
+
+module.exports.socketManager  = {
+  init: (httpServer) => {
+    io = require('socket.io')(httpServer, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+    });
+    return io;
+  },
+  getIO: () => {
+    if (!io) {
+      throw new Error('Socket.io not initialized!')
+    }
+    return io;
+  }
+};
