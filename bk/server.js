286eb34f5f3a8adb03d2fdbcb2e0e74d49385fc6
Scvotr <stasforeva23@mail.ru>
2024-02-27 16:30:11 +0300

    moved the server creation event outside the handler

--- bk/server.js
+++ bk/server.js
@@ -1,16 +1,23 @@
 'use strict'
 
 const http = require('http')
-require('dotenv').config();
+require('dotenv').config()
 
-const server = http.createServer(async (req, res) => {
+const server = http.createServer(async (req, res) => {})
 
-  server.on("error", (error) => {
-    console.log("server.on ", error)
-  })
-
-  server.listen({ host, port }, () => {
-    const address = server.address();
-    console.log(`Сервер запущен на адресе ${address.address}:${address.port}`);
-  })
+server.on("error", (error) => {
+  console.log("server.on ", error)
 })
+
+const host = process.env.HOST || "localhost"
+const port = process.env.PORT || 3070
+
+server.listen({ host,port }, () => {
+  const address = server.address()
+  console.log(`Сервер запущен на адресе ${address.address}:${address.port}`)
+})
+
+// $env:HOST="192.168.8.102"; $env:PORT="3050"; node .\server.js // win PowerShell 
+// $env:HOST="localhost"; $env:PORT="3070"; node .\server.js     // win PowerShell 
+// export HOST=192.168.8.102 export PORT=3050 node ./server.js   // Unix и Linux
+// export HOST=localhost export PORT=3070 node ./server.js       // Unix и Linux
