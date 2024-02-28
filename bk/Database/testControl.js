691d50fc56e198f3803c5238025686d6badf75c0
Scvotr <stasforeva23@mail.ru>
2024-02-28 16:50:23 +0300

    move to util function

--- bk/Database/testControl.js
+++ bk/Database/testControl.js
@@ -1,32 +1,13 @@
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('../database.db')
-const logger = require('../utils/logger/logger');
-const { openConnection, closeConnection } = require('./utils/activeConnections/activeConnectionsCount')
 
 const initDatabase = async() => {}
 
-const executeDatabaseQueryAsync = async (command, params = [], method = 'all') => {
-  logger.info(`Executing query: ${command} with params: ${JSON.stringify(params)}`);
-  openConnection(db);
-  return new Promise((resolve, reject) => {
-    db[method](command, params, (err, result) => {
-      if (err) {
-        logger.error(`Error executing query: ${err}`);
-        reject(err);
-      } else {
-        resolve(result);
-      }
-      closeConnection(db); // Ensure connection is closed after query execution
-    });
-  });
-};
+db.serialize(async () => {
 
-// db.serialize(async () => {
-//   createTableTasksComments()
-// })
+})
 
 module.exports ={
   initDatabase,
-  executeDatabaseQueryAsync
 }
 
