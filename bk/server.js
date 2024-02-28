11bfd8c7bf39573293a0686ed93b5c59538d1837
Scvotr <stasforeva23@mail.ru>
2024-02-28 03:54:56 +0300

    add router handlers

--- bk/server.js
+++ bk/server.js
@@ -2,11 +2,37 @@
 
 const http = require('http')
 require('dotenv').config()
-const {socketManager} = require('./utils/socket/socketManager')
+const { socketManager } = require('./utils/socket/socketManager')
 const { setupSocket } = require('./socketService')
 const socketEventLogger = require('./utils/socket/socketEventLogger')
 
-const server = http.createServer(async (req, res) => {})
+const server = http.createServer(async (req, res) => {
+  try {
+    const { url, method } = req;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    
+    if (method === "OPTIONS") {
+      handleOptionsRequest(req, res);
+    } else {
+      let routeHandled = false;
+      for (const { prefix, handler } of routeHandlers) {
+        if (url.startsWith(prefix)) {
+          await handler(req, res);
+          routeHandled = true;
+          break;
+        }
+      }
+      if (!routeHandled) {
+        handleDefaultRoute(req, res);
+      }
+    }
+  } catch (error) {
+    logger.error(`Server-error: ${error}`)
+    console.log("server-catch-error: ", error);
+  }
+})
 
 server.on("error", (error) => {
   console.log("server.on ", error)
