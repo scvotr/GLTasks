c2bf6233f1f592e0898456cedfb924b2fb0b7deb
Scvotr <stasforeva23@mail.ru>
2024-02-28 04:54:10 +0300

    add error handling

--- bk/routingHandlers/handleAuthRoutes.js
+++ bk/routingHandlers/handleAuthRoutes.js
@@ -1,28 +1,38 @@
 'use strict'
 
+const logger = require('../utils/logger/logger')
+
 const handleAuthRoutes = async (req, res) => {
+  req.on('error', (err) => {
+    console.error('handleAuthRoutes! Ошибка в запросе:', err)
+    logger.error('handleAuthRoutes! Ошибка в запросе:', err)
+    res.statusCode = 400
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ error: 'Ошибка в запросе' }))
+  });
+
   const { url, method } = req;
   
   try {
     if (url.startsWith("/auth")) {
-      const routeHandler = routeHandlers[url];
+      const routeHandler = routeHandlers[url]
       if (routeHandler) {
         if (method === "POST") {
-          await routeHandler(req, res);
+          await routeHandler(req, res)
         } else {
-          handleDefaultRoute(req, res);
+          handleDefaultRoute(req, res)
         }
       } else {
-        handleDefaultRoute(req, res);
+        handleDefaultRoute(req, res)
       }
     } else {
-      handleDefaultRoute(req, res);
+      handleDefaultRoute(req, res)
     }
   } catch (error) {
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ error: "handleAuthRoutes - ERROR" }));
+    res.statusCode = 500
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ error: "handleAuthRoutes - ERROR" }))
   }
-};
+}
 
 module.exports = { handleAuthRoutes };
