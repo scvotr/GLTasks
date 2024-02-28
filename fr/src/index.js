251cd63c1acbfa67b0a0c81489313eebe091ae1b
Scvotr <stasforeva23@mail.ru>
2024-02-28 07:16:42 +0300

    add routes in app

--- fr/src/index.js
+++ fr/src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './routes/routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+  <RouterProvider router={routes}/>
 );
