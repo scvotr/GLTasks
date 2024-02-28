df6283ca617336bcc81b1aa6a05bf028eb8c2abe
Scvotr <stasforeva23@mail.ru>
2024-02-28 05:13:27 +0300

    add for get data from req

--- bk/controllers/Auth/AuthControler.js
+++ bk/controllers/Auth/AuthControler.js
@@ -5,11 +5,13 @@ const logger = require('../../utils/logger/logger')
 class AuthControler {
   async registrationLocalUser(req, res) {
     try {
-      
+      res.statusCode = 200
+      res.setHeader("Content-Type", "application/json")
+      const postPayload = await getPostDataset(req)
     } catch (error) {
       console.error('Ошибка при регистрации локального пользователя', error)
       logger.error('Ошибка при регистрации локального пользователя', error)
-      res.statusCode = 500;
+      res.statusCode = 500
       res.end(
         JSON.stringify({ error: "Ошибка при регистрации локального пользователя." })
       );
