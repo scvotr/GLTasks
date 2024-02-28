6f212392e0c27943addaff106178620ade99ff31
Scvotr <stasforeva23@mail.ru>
2024-02-28 05:07:14 +0300

    add method to reg local user & error handling

--- bk/controllers/Auth/AuthControler.js
+++ bk/controllers/Auth/AuthControler.js
@@ -1,7 +1,20 @@
 'use strict'
 
-class AuthControler {
+const logger = require('../../utils/logger/logger')
 
+class AuthControler {
+  async registrationLocalUser(req, res) {
+    try {
+      
+    } catch (error) {
+      console.error('Ошибка при регистрации локального пользователя', error)
+      logger.error('Ошибка при регистрации локального пользователя', error)
+      res.statusCode = 500;
+      res.end(
+        JSON.stringify({ error: "Ошибка при регистрации локального пользователя." })
+      );
+    }
+  }
 }
 
 module.exports = new AuthControler();
