8507889dd1e67144c874956853d389e343e8fcaa
Scvotr <stasforeva23@mail.ru>
2024-02-28 04:32:17 +0300

    change import method

--- bk/routingHandlers/handleDefaultRoute.js
+++ bk/routingHandlers/handleDefaultRoute.js
@@ -1,9 +1,11 @@
 'use strict'
 
-export const handleDefaultRoute = (req, res) => {
+const handleDefaultRoute = (req, res) => {
   if (req.method === "GET" || req.method === "POST") {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ message: "Not found 404!" }));
+    res.statusCode = 404
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ message: "Not found 404!" }))
   }
 }
+
+module.exports = {handleDefaultRoute}
