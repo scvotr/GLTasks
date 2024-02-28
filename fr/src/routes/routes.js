2c37dc18eebc6c70ac3ee1e8093739c2f36f7f22
Scvotr <stasforeva23@mail.ru>
2024-02-28 10:24:10 +0300

    add route for login cmp

--- fr/src/routes/routes.js
+++ fr/src/routes/routes.js
@@ -1,10 +1,16 @@
 import App from "../App";
 import { createBrowserRouter } from "react-router-dom";
+import { Login } from "../components/Authtorization/Login/Login";
 
 export const routes = createBrowserRouter([
   {
     path: '/',
     element: <App/>,
-    children: []
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+    ]
   }
 ])
