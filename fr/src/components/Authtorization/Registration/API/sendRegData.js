2417d05140472698a85503ff6e3c87d21c01b73f
Scvotr <stasforeva23@mail.ru>
2024-02-28 14:16:34 +0300

    add fetch for reg user

---
+++ fr/src/components/Authtorization/Registration/API/sendRegData.js
@@ -0,0 +1,24 @@
+export const sendRegData = async (regData, hostAddr, resError) => {
+  try {
+    const res = await fetch(hostAddr + "/auth/registrationWithoutMail", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(regData),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      const token = res.headers.get("Authorization");
+      if (token === null) {
+        console.log("Token not found!", data.Registration);
+        resError(data.Registration)
+      } else {
+        // localStorage.setItem("token", token);
+        window.location.href = "/Login";
+      }
+    } else {
+      resError(data.Registrtaion)
+    }
+  } catch (error) {
+    resError(error)
+  }
+};
