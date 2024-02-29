9242a87288c07acc96026a9072c225b7c2f7eaca
Scvotr <stasforeva23@mail.ru>
2024-02-29 07:37:49 +0300

    init method & add role

---
+++ bk/Database/models/User/userRoles.js
@@ -0,0 +1,23 @@
+const { executeDatabaseQueryAsync, queryAsyncWraper } = require("../../utils/executeDatabaseQuery/executeDatabaseQuery")
+
+const createUserRoles = async () => {
+  try {
+    await executeDatabaseQueryAsync(
+      // command
+      `CREATE TABLE IF NOT EXISTS user_roles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        role TEXT NOT NULL
+       )`, []
+    )
+    const rows = await queryAsyncWraper('SELECT COUNT(*) FROM user_roles', 'get');
+    if (rows['COUNT(*)'] === 0) { // Если записей нет, то выполняем вставку начальных значений
+      await queryAsyncWraper("INSERT INTO user_roles (role) VALUES ('admin'), ('new'), ('guest'), ('user'), ('lead'), ('general')", 'run');
+    }
+  } catch (error) {
+    console.log('DB ERROR - createUserRoles: ', error)
+  }
+}
+
+module.exports ={
+  createUserRoles
+}
