ee1d4e8bd74e11a7acbfb934603df6a4fb126b9b
Scvotr <stasforeva23@mail.ru>
2024-02-29 07:41:52 +0300

    rm commenst

--- bk/Database/models/OrgStructure/subdepartments.js
+++ bk/Database/models/OrgStructure/subdepartments.js
@@ -9,9 +9,8 @@ const createTableSubdepartments = async () => {
         name TEXT NOT NULL,
         FOREIGN KEY (department_id) REFERENCES departments(id)
       )`, [])
-    // Выполняем проверку наличия записей в таблице
     const rows = await queryAsyncWraper('SELECT COUNT(*) FROM subdepartments ', 'get');
-    if (rows['COUNT(*)'] === 0) { // Если записей нет, то выполняем вставку начальных значений
+    if (rows['COUNT(*)'] === 0) { 
       await queryAsyncWraper("INSERT INTO subdepartments (department_id, name) VALUES (1, 'новый'), (2, 'ХПР'), (3, 'Служба Гл. Инженера'), (4, 'Служба Гл. Инженера'), (3, 'Служба Качества'), (4, 'Служба Качества')", 'run');
     }
   } catch (error) {
