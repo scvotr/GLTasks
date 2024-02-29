0d81844d9e30fbd06cc9bab05ef690722f561e6d
Scvotr <stasforeva23@mail.ru>
2024-02-29 05:56:49 +0300

    init task status

--- bk/Database/initDatabase.js
+++ bk/Database/initDatabase.js
@@ -4,6 +4,7 @@ const { createTableTasksComments } = require('./models/Task/tasksComments')
 const { createTableDepartments } = require('./models/OrgStructure/departments')
 const { createTablePositions } = require('./models/OrgStructure/positions')
 const { createTableSubdepartments } = require('./models/OrgStructure/subdepartments')
+const { createTaskStatus } = require('./models/Task/taskStatus')
 
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('../database.db')
@@ -12,6 +13,7 @@ const initDatabase = async() => {}
 
 db.serialize(async () => {
   createTableTasks()
+  createTaskStatus()
   createTableTaskReadStatus()
   createTableTasksComments()
   createTableDepartments()
