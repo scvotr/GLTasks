7beee267de5a55df839f1c10d7d5853676e01ce8
Scvotr <stasforeva23@mail.ru>
2024-03-01 05:37:54 +0300

    md function for test

--- bk/Database/utils/executeDatabaseQuery/executeDatabaseQuery.js
+++ bk/Database/utils/executeDatabaseQuery/executeDatabaseQuery.js
@@ -52,3 +52,28 @@ module.exports ={
   queryAsyncWraper,
   queryAsyncWraperParam,
 }
+
+
+// const executeDatabaseQueryAsync = async (command, params = [], method = 'all') => {
+//   if (!Array.isArray(params)) {
+//     throw new Error('Params must be an array');
+//   }
+
+//   if (method !== 'all' && method !== 'get' && method !== 'run') {
+//     throw new Error('Unsupported method');
+//   }
+
+//   logger.info(`Executing query: ${command} with params: ${JSON.stringify(params)}`);
+//   openConnection(db);
+//   return new Promise((resolve, reject) => {
+//     db[method](command, params, (err, result) => {
+//       if (err) {
+//         logger.error(`Error executing query: ${err}`);
+//         reject(err);
+//       } else {
+//         resolve(result);
+//       }
+//       closeConnection(db); // Ensure connection is closed after query execution
+//     });
+//   });
+// };
