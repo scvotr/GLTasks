c8b3a3cbac8970f9e9c1c6218f2a769c298223a9
Scvotr <stasforeva23@mail.ru>
2024-03-01 07:51:43 +0300

    add handler for response auth data from server

--- fr/src/context/AuthProvider.js
+++ fr/src/context/AuthProvider.js
@@ -1,7 +1,47 @@
-const { createContext, useContext } = require("react");
+const { createContext, useContext, useState, useCallback } = require("react");
 
 const AuthContext = createContext()
 
 export const useAuthContext = () => {
   return useContext(AuthContext)
+}
+
+export const AuthProvider = ({children}) => {
+  const [id, setId] = useState('')
+  const [role, setRole] = useState('')
+  const [roleRef, setRoleRef] = useState('')
+  const [name, setName] = useState('')
+  const [token, setToken] = useState('')
+  const [dep, setDep] = useState('')
+  const [subDep, setSubDep] = useState('')
+  const [position, setPosition] = useState('')
+  const [login, setLogin] = useState(false)
+  
+
+  const handleServerResponse = useCallback((response) => {
+    console.log(response)
+    if(response) {
+      setId(response.id)
+      setRole(response.role)
+      setRoleRef(response.role_ref)
+      setName(response.name)
+      setDep(response.dep)
+      setSubDep(response.subDep)
+      setPosition(response.position)
+      setToken(response.token)
+      setLogin(true)
+    } else {
+      setLogin(false)
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        handleServerResponse
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
 }
