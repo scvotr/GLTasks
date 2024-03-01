3ba88f3ea50b79a3c43619acb4e9710d76d14c74
Scvotr <stasforeva23@mail.ru>
2024-03-01 05:43:26 +0300

    add tokens query

---
+++ bk/Database/queries/Auth/tokenQuery.js
@@ -0,0 +1,95 @@
+'use strict'
+const { executeDatabaseQueryAsync } = require("../../utils/executeDatabaseQuery/executeDatabaseQuery")
+
+const addTokenQ = async (userId, token) => {
+  try {
+    const command = `INSERT INTO tokens(user_id, token) VALUES (?, ?)`
+    await executeDatabaseQueryAsync(command, [userId, token], `run`)
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+const updateTokenQ = async (userId, token) => {
+  try {
+    const command = `UPDATE tokens SET token = ? WHERE user_id = ?`
+    await executeDatabaseQueryAsync(command, [userId, token], `run`)
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+const getTokenQ = async (userId) => {
+  try {
+    const command = `SELECT * FROM tokens WHERE user_id = ?`
+    return await executeDatabaseQueryAsync(command, [userId], `run`)
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+const deleteTokenQ = async (userId) => {
+  try {
+    const command = `DELETE FROM tokens WHERE user_id = ?`
+    await executeDatabaseQueryAsync(command, [userId], `run`)
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+module.exports = {
+  addTokenQ,
+  updateTokenQ,
+  getTokenQ,
+  deleteTokenQ,
+}
+// ! РАБОЧИЙ ВАРИАНТ нужно попробывать
+// class TokenRepository {
+//   async addToken(userId, token) {
+//     try {
+//       const command = `INSERT INTO tokens(user_id, token) VALUES (?, ?)`;
+//       await executeDatabaseQueryAsync(command, [userId, token], "run");
+//     } catch (error) {
+//       throw new Error("Ошибка запроса к базе данных");
+//     }
+//   }
+
+//   async updateToken(userId, token) {
+//     try {
+//       const command = `UPDATE tokens SET token = ? WHERE user_id = ?`;
+//       await executeDatabaseQueryAsync(command, [token, userId], "run");
+//     } catch (error) {
+//       throw new Error("Ошибка запроса к базе данных");
+//     }
+//   }
+
+//   async getToken(userId) {
+//     try {
+//       const command = `SELECT * FROM tokens WHERE user_id = ?`;
+//       const result = await executeDatabaseQueryAsync(command, [userId], "run");
+//       return result;
+//     } catch (error) {
+//       throw new Error("Ошибка запроса к базе данных");
+//     }
+//   }
+
+//   async deleteToken(userId) {
+//     try {
+//       const command = `DELETE FROM tokens WHERE user_id = ?`;
+//       await executeDatabaseQueryAsync(command, [userId], "run");
+//     } catch (error) {
+//       throw new Error("Ошибка запроса к базе данных");
+//     }
+//   }
+// }
+
+// const TokenRepository = require("./TokenRepository");
+
+// const tokenRepo = new TokenRepository();
+
+// // Пример использования операций CRUD
+
+// await tokenRepo.addToken(userId, token); // Создание новой записи
+// await tokenRepo.updateToken(userId, newToken); // Обновление записи
+// const token = await tokenRepo.getToken(userId); // Получение записи
+// await tokenRepo.deleteToken(userId); // Удаление записи
