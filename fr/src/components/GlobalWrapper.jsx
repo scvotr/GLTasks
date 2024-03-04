2e82d31331b456b889b83fa8753dc72a1d4bf622
Scvotr <stasforeva23@mail.ru>
2024-03-04 15:09:10 +0300

    add admin layout

--- fr/src/components/GlobalWrapper.jsx
+++ fr/src/components/GlobalWrapper.jsx
@@ -1,7 +1,28 @@
+import { useAuthContext } from "../context/AuthProvider"
+import { AdminLayout } from "./Layouts/AdminLayout/AdminLayout"
 import { DefaultLayoutMain } from "./Layouts/DefaultLayoutMain/DefaultLayoutMain"
+import { LefSideAdmin } from "./Navigation/Admin/LefSideAdmin/LefSideAdmin"
+
 
 export const GlobalWrapper = () => {
+  const currentUser = useAuthContext()
+
+  const comtentMap = new Map([
+    ['admin', ()=><LefSideAdmin> <AdminLayout/> </LefSideAdmin>]
+  ])
+
+  const renderContent = () => {
+    if(currentUser && currentUser.login) {
+      const getContentByRole = comtentMap.get(currentUser.role)
+      return getContentByRole ? getContentByRole() : <DefaultLayoutMain />
+    } else {
+      return <DefaultLayoutMain />
+    }
+  }
+
   return (
-    <DefaultLayoutMain />
+    <>
+      {renderContent()}
+    </>
   )
 }
