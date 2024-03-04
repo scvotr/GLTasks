e2c9fc104d1ae5c008eddab688a9d6be6b958d7e
Scvotr <stasforeva23@mail.ru>
2024-03-04 16:28:34 +0300

    add cmp for users

--- fr/src/components/Navigation/Admin/Menu/Users/NewUsers.jsx
+++ fr/src/components/Navigation/Admin/Menu/Users/NewUsers.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react"
+import { AppBar, Toolbar, Typography } from "@mui/material"
+
 
 export const NewUsers = () => {
   return (
     <>
-      users
+      <AppBar
+        position="static"
+        sx={{
+          mt: 2,
+          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+          border: "1px solid #e0e0e0",
+          borderRadius: "5px",
+        }}>
+        <Toolbar>
+          <Typography>Пользователи: </Typography>
+        </Toolbar>
+      </AppBar>
     </>
   )
 }
