b920f8f55054e51b0c2e4972f8680224c4744c96
Scvotr <stasforeva23@mail.ru>
2024-03-04 16:50:19 +0300

    add route for get all users

---
+++ bk/routingHandlers/handleAdminRoutes.js
@@ -0,0 +1,42 @@
+const UserControler = require("../controllers/Admin/UserControler")
+// const DepsControler = require('../controls/Admin/Deps/DepsControler')
+const { handleDefaultRoute } = require("../routingHandlers/handleDefaultRoute")
+const { protectRouteTkPl } = require("../utils/protectRouteTkPl")
+
+const routeHandlers = {
+  "/admin/getAllUsers": UserControler.getAllUsers,
+  // "/admin/updateUserData": UserControler.updateUserData,
+  // "/admin/getAllTasks": UserControler.getAllTasks,
+  // "/admin/createNewDep": DepsControler.createNewDep,
+  // "/admin/removeDep": DepsControler.removeDep,
+  // "/admin/createNewSubDep": DepsControler.createNewSubDep,
+  // "/admin/createNewPosition": DepsControler.createNewPosition,
+};
+
+const handleAdminRoutes = async (req, res) => {
+  const { url, method } = req;
+  
+  try {
+    if (url.startsWith("/admin")) {
+      const routeHandler = routeHandlers[url];
+      if (routeHandler) {
+        if (method === "POST") {
+          await protectRouteTkPl(routeHandler)(req, res);
+        } else {
+          handleDefaultRoute(req, res);
+        }
+      } else {
+        handleDefaultRoute(req, res);
+      }
+    } else {
+      handleDefaultRoute(req, res);
+    }
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "handleAdminRoutes - ERROR" }));
+  }
+};
+
+module.exports = { handleAdminRoutes };
