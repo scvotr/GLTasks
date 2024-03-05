1a4d045495e74e62899f5548d3341ab1f3594707
Scvotr <stasforeva23@mail.ru>
2024-03-05 10:20:25 +0300

    add ful method

--- bk/controllers/orgStruct/OrgStructControler.js
+++ bk/controllers/orgStruct/OrgStructControler.js
@@ -1,7 +1,26 @@
+'use strict'
+
+const { getAllDepartmentsQ } = require("../../Database/queries/OrgStructure/departmensQuery")
 
 class OrgStructControler {
   async getDepartments(req, res) {
-    
+    try {
+      const authDecodeUserData = req.user
+      const data = await getAllDepartmentsQ()
+      if (data.length === 0) {
+        res.statusCode = 204
+      } else {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.write(JSON.stringify(data))
+      }
+      res.end()
+    } catch (error) {
+      res.statusCode = 500
+      res.end(JSON.stringify({
+        error: 'getDepartments'
+      }))
+    }
   }
 }
 
