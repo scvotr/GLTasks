cb4dcb4d2f5f5320d8dacb2aa12d84625adca737
Scvotr <stasforeva23@mail.ru>
2024-03-05 09:15:49 +0300

    rm menu

--- fr/src/components/Navigation/Admin/Tables/UsersTable/UsersTableView.jsx
+++ fr/src/components/Navigation/Admin/Tables/UsersTable/UsersTableView.jsx
@@ -4,6 +4,8 @@ import { useState } from "react"
 import Menu from "@mui/material/Menu"
 import MenuItem from "@mui/material/MenuItem"
 import Divider from "@mui/material/Divider"
+import { ModalCustom } from "../../../../ModalCustom/ModalCustom"
+import { EditUserForm } from "../../EditUserForm/EditUserForm"
 
 const columns = [
   {
@@ -58,75 +60,66 @@ const columns = [
 
 export const UsersTableView = ({ actionType, users }) => {
   const [actionTypeTS, setActionTypeTS] = useState([])
-  const [anchorEl, setAnchorEl] = useState(null)
-  const open = Boolean(anchorEl)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [selectedUser, setSelectedUser] = useState(null)
+
+  const openModal = user => {
+    setSelectedUser(user)
+    setModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setSelectedUser(null)
+    setModalOpen(false)
+  }
 
   const handleCellClick = (params, event) => {
     console.log("Кликнута ячейка:", params.field, params.row.id, params.row, params.row.task_status)
     setActionTypeTS(params.row.task_status)
-    // openModal(params.row)
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
+    openModal(params.row)
   }
 
   return (
     <>
-      <Menu
-        id="positioned-menu"
-        aria-labelledby="positioned-button"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}>
-        <MenuItem onClick={handleClose}>Назначить отдел</MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose}>Редактировать</MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose}>Удалить</MenuItem>
-      </Menu>
-      {/*  */}
-      <Box
-        sx={{
-          height: 500,
-          width: "100%",
-          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-          border: "1px solid #e0e0e0",
-          borderRadius: "5px",
-        }}>
-        <DataGrid
-          rowHeight={25}
-          rows={
-            users && users
-              ? users.map(user => ({
-                  ...user,
-                  id: user.id,
-                }))
-              : []
-          }
-          columns={columns}
-          localeText={ruRU.components.MuiDataGrid.defaultProps.localeText}
-          onCellClick={handleCellClick}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 15 },
-            },
-            sorting: {
-              sortModel: [{ field: "created_on", sort: "desc" }], // Направление сортировки: 'asc' - по возрастанию, 'desc' - по убыванию
-            },
-          }}
-          pageSizeOptions={[15, 20, 25]}
-        />
-      </Box>
+      <>
+        {selectedUser && (
+          <ModalCustom isOpen={modalOpen} onClose={closeModal}>
+            <EditUserForm user={selectedUser} />
+          </ModalCustom>
+        )}
+        <Box
+          sx={{
+            height: 500,
+            width: "100%",
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+            border: "1px solid #e0e0e0",
+            borderRadius: "5px",
+          }}>
+          <DataGrid
+            rowHeight={25}
+            rows={
+              users && users
+                ? users.map(user => ({
+                    ...user,
+                    id: user.id,
+                  }))
+                : []
+            }
+            columns={columns}
+            localeText={ruRU.components.MuiDataGrid.defaultProps.localeText}
+            onCellClick={handleCellClick}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 15 },
+              },
+              sorting: {
+                sortModel: [{ field: "created_on", sort: "desc" }], // Направление сортировки: 'asc' - по возрастанию, 'desc' - по убыванию
+              },
+            }}
+            pageSizeOptions={[15, 20, 25]}
+          />
+        </Box>
+      </>
     </>
   )
 }
