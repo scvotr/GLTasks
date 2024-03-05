4c2e1973ba1c92a548d00547fe3068b0b79ac195
Scvotr <stasforeva23@mail.ru>
2024-03-05 07:05:14 +0300

    add get all user method

--- bk/controllers/Admin/UserControler.js
+++ bk/controllers/Admin/UserControler.js
@@ -1,15 +1,36 @@
 'use strict'
 
+const jwt = require("jsonwebtoken")
+const { getAllUsers } = require("../../Database/queries/User/adminQuery")
+require("dotenv").config()
+const SECRET_KEY = process.env.KEY_TOKEN
 
 class UserControler {
   async getAllUsers(req, res) {
     try {
-      
+      const authDecodeUserData = req.user
+      if(authDecodeUserData.role !== "admin") {
+        return res.end(
+          JSON.stringify({
+            getAllUsers: "Нет прав на доступ",
+          })
+        )  
+      }
+      const data = await getAllUsers()
+      data.length === 0 ? res.statusCode = 204 :
+      res.statusCode = 200
+      res.setHeader("Content-Type", "application/json")
+      res.write(JSON.stringify(data))
+      res.end()
     } catch (error) {
-      
+      res.statusCode = 500;
+      res.end(
+        JSON.stringify({
+          error: "getAllUsers",
+        })
+      );
     }
   }
 }
-
 
 module.exports = new UserControler()
