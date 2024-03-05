1aa47a85de64c3c6afd23f15c4147230cdcc39bf
Scvotr <stasforeva23@mail.ru>
2024-03-05 10:36:59 +0300

    add method

--- bk/controllers/orgStruct/OrgStructControler.js
+++ bk/controllers/orgStruct/OrgStructControler.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { getAllDepartmentsQ } = require("../../Database/queries/OrgStructure/departmensQuery")
+const { getSubDepartmentsByIDQ } = require("../../Database/queries/OrgStructure/subDepartmetsQuery")
 
 class OrgStructControler {
   async getDepartments(req, res) {
@@ -19,6 +20,26 @@ class OrgStructControler {
       res.statusCode = 500
       res.end(JSON.stringify({
         error: 'getDepartments'
+      }))
+    }
+  }
+  async getSubDepartmentsByID(req, res) {
+    try {
+      const dep_id = req.user.payLoad
+
+      const data = await getSubDepartmentsByIDQ(dep_id)
+      if (data.length === 0) {
+        res.statusCode = 204
+      } else {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.write(JSON.stringify(data))
+      }
+      res.end()
+    } catch (error) {
+      res.statusCode = 500
+      res.end(JSON.stringify({
+        error: 'getSubDepartmentsByID'
       }))
     }
   }
