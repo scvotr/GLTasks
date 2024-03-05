53d631bc652f035cb8326f2f9cb47ca3a116f4a8
Scvotr <stasforeva23@mail.ru>
2024-03-05 10:11:00 +0300

    add routes handler OrgStructure

---
+++ bk/routingHandlers/handleOrgStructRoutes.js
@@ -0,0 +1,45 @@
+const OrgStructControler = require('../controllers/orgStruct/OrgStructControler');
+const { handleDefaultRoute } = require("../routingHandlers/handleDefaultRoute");
+const { protectRouteTkPl } = require("../utils/protectRouteTkPl");
+
+const routeHandlers = {
+  "/orgStruct/getDepartments": OrgStructControler.getDepartments,
+  // "/orgStruct/getDepartmentsByID": OrgStructControler.getDepartmentsByID,
+  // "/orgStruct/getDepartmentsFrom": OrgStructControler.getDepartmentsFrom,
+  // "/orgStruct/getSubDepartments": OrgStructControler.getSubDepartments,
+  // "/orgStruct/getSubDepartmentsByID": OrgStructControler.getSubDepartmentsByID,
+  // "/orgStruct/getPositions": OrgStructControler.getPositions,
+  // "/orgStruct/getPositionsByID": OrgStructControler.getPositionsByID,
+  // "/orgStruct/getAllWorkshops": OrgStructControler.getAllWorkshops,
+  // "/orgStruct/getWorkshopsByDepID": OrgStructControler.getWorkshopsByDepID,
+};
+
+const handleOrgStructRoutes = async (req, res) => {
+  const { url, method } = req;
+  
+  try {
+    if (url.startsWith("/orgStruct")) {
+      if (method === "POST") {
+        const routeHandler = routeHandlers[url];
+        if (routeHandler) {
+          await protectRouteTkPl(routeHandler)(req, res);
+        } else {
+          handleDefaultRoute(req, res);
+        }
+      } else {
+        handleDefaultRoute(req, res);
+      }
+    } else {
+      handleDefaultRoute(req, res);
+    }
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "handleOrgStructRoutes - ОШИБКА" }));
+  }
+};
+
+module.exports = {
+  handleOrgStructRoutes
+};
