5c2b2b1107b36503acf95680eabd9e97ee7a3ffc
Scvotr <stasforeva23@mail.ru>
2024-03-05 07:57:04 +0300

    add main cmp for user menu

---
+++ fr/src/components/Navigation/Admin/Menu/Users/Users.jsx
@@ -0,0 +1,21 @@
+import { AppBar, Toolbar, Typography } from "@mui/material"
+
+export const Users = () => {
+  return (
+    <>
+      <AppBar
+        position="static"
+        sx={{
+          mt: 2,
+          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+          border: "1px solid #e0e0e0",
+          borderRadius: "5px",
+        }}>
+        <Toolbar>
+          <Typography>Пользователи: </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* <UsersTableView users={allUsers} /> */}
+    </>
+  )
+}
