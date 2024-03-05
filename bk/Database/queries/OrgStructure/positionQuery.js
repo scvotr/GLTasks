95122fa0c0ab89e5736281e69bd0f07f56816276
Scvotr <stasforeva23@mail.ru>
2024-03-05 16:09:33 +0300

    add query for all position

--- bk/Database/queries/OrgStructure/positionQuery.js
+++ bk/Database/queries/OrgStructure/positionQuery.js
@@ -1,5 +1,7 @@
 'use strict'
-const { executeDatabaseQueryAsync } = require("../../utils/executeDatabaseQuery/executeDatabaseQuery")
+const {
+  executeDatabaseQueryAsync
+} = require("../../utils/executeDatabaseQuery/executeDatabaseQuery")
 
 const getPositionsByIDQ = async (subDep_id) => {
   try {
@@ -10,6 +12,37 @@ const getPositionsByIDQ = async (subDep_id) => {
   }
 }
 
+const getPositionsQ = async (subDep_id) => {
+  try {
+    const command = `
+    SELECT p.id, p.name AS position_name, d.name AS department_name, sd.name AS subdepartment_name
+    FROM positions p
+    LEFT JOIN departments d ON p.department_id = d.id
+    LEFT JOIN subdepartments sd ON p.subdepartment_id = sd.id;
+    `
+    return await executeDatabaseQueryAsync(command, [])
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+const getPositionsBySubDepIdQ = async (subDep_id) => {
+  try {
+    const command = `
+    SELECT p.id, p.name AS position_name, d.name AS department_name, sd.name AS subdepartment_name
+    FROM positions p
+    LEFT JOIN departments d ON p.department_id = d.id
+    LEFT JOIN subdepartments sd ON p.subdepartment_id = sd.id
+    WHERE p.subdepartment_id = ?;
+    `
+    return await executeDatabaseQueryAsync(command, [subDep_id])
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
 module.exports = {
   getPositionsByIDQ,
+  getPositionsQ,
+  getPositionsBySubDepIdQ,
 }
