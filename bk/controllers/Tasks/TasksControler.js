089a3c8e6566cfb939ffd732276bf2be9a583561
Scvotr <stasforeva23@mail.ru>
2024-03-06 16:42:00 +0300

    add get files and save to folder

--- bk/controllers/Tasks/TasksControler.js
+++ bk/controllers/Tasks/TasksControler.js
@@ -1,15 +1,24 @@
 'use strict'
 
+const { saveAndConvert } = require("../../utils/files/saveAndConvert");
+
 class TasksControler {
   async addNewTask(req, res) {
     const authDecodeUserData = req.user
     const user_id = authDecodeUserData.id
-    const postPayload = authDecodeUserData.payLoad
-    console.log(postPayload)
+    const postPayload = authDecodeUserData.payLoad; console.log(postPayload.fields)
     const fields = postPayload.fields;
     const files = postPayload.files
     const taskFolder = fields.task_id
     const fileNames = [];
+    for (const [key, file] of Object.entries(files)) {
+      try {
+        const fileName = await saveAndConvert(file, taskFolder)
+        fileNames.push(fileName.fileName);
+      } catch (error) {
+        console.error('Error saving file:', error);
+      }
+    }
   }
 }
 
