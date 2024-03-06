7c4863d39ade91bfcb8d169a6a056d9a381a2c4a
Scvotr <stasforeva23@mail.ru>
2024-03-06 07:25:14 +0300

    add get all position by endpoint

--- fr/src/components/Navigation/Admin/Menu/Struct/NewStruct.jsx
+++ fr/src/components/Navigation/Admin/Menu/Struct/NewStruct.jsx
@@ -1,14 +1,26 @@
 import { useCallback, useEffect, useState } from "react"
-import { AppBar, Toolbar, Typography } from "@mui/material"
+import AddIcon from "@mui/icons-material/Add"
+import { AppBar, Box, Fab, Toolbar, Typography } from "@mui/material"
 import { useAuthContext } from "../../../../../context/AuthProvider"
 import { getDataFromEndpoint } from "../../../../../utils/getDataFromEndpoint"
 import { StructTable } from "../../Tables/StructTable/StructTableView"
+import { ModalCustom } from "../../../../ModalCustom/ModalCustom"
+import { AddStuctForm } from "./AddStuctForm"
 
 export const NewStruct = () => {
   const currentUser = useAuthContext()
   const [reqStatus, setReqStatus] = useState({ loading: true, error: null })
   const [dataFromEndpoint, setDataFromEndpoint] = useState([])
-  const [formKey, setFormKey] = useState(0);
+  const [formKey, setFormKey] = useState(0)
+
+  const [modalOpen, setModalOpen] = useState(false)
+  const openModal = user => {
+    setModalOpen(true)
+  }
+  const closeModal = () => {
+    setModalOpen(false)
+    // reRender(prevKey => prevKey + 1)
+  }
 
   const fetchData = useCallback(async () => {
     if (currentUser.login) {
@@ -23,23 +35,37 @@ export const NewStruct = () => {
     }
   }, [currentUser, formKey])
 
-  useEffect(() => {fetchData()}, [fetchData])
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
 
   return (
     <>
-      <AppBar
-        position="static"
-        sx={{
-          mt: 2,
-          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-          border: "1px solid #e0e0e0",
-          borderRadius: "5px",
-        }}>
-        <Toolbar>
-          <Typography>Отделы: </Typography>
-        </Toolbar>
-      </AppBar>
-      <StructTable users={dataFromEndpoint} reRender={setFormKey}/>
+      <>
+        <ModalCustom isOpen={modalOpen} onClose={closeModal} infoText='Добавить отдел'>
+          <AddStuctForm  onTaskSubmit={closeModal}/>
+        </ModalCustom>
+        <Box>
+          <AppBar
+            position="static"
+            sx={{
+              mt: 2,
+              boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+              border: "1px solid #e0e0e0",
+              borderRadius: "5px",
+            }}>
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Отделы:{" "}
+              </Typography>
+              <Fab color="secondary" aria-label="add" onClick={openModal}>
+                <AddIcon />
+              </Fab>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <StructTable users={dataFromEndpoint} reRender={setFormKey} />
+      </>
     </>
   )
 }
