09c37e1a4b22463440a090d818e45df733d452e1
Scvotr <stasforeva23@mail.ru>
2024-03-06 08:31:39 +0300

    add reder cmp for chife

--- fr/src/components/GlobalWrapper.jsx
+++ fr/src/components/GlobalWrapper.jsx
@@ -1,13 +1,16 @@
 import { useAuthContext } from "../context/AuthProvider"
 import { AdminLayout } from "./Layouts/AdminLayout/AdminLayout"
 import { DefaultLayoutMain } from "./Layouts/DefaultLayoutMain/DefaultLayoutMain"
+import { LeadLayout } from "./Layouts/LeadLayout/LeadLayout"
 import { LefSideAdmin } from "./Navigation/Admin/LefSideAdmin/LefSideAdmin"
+import { LeftSideDrawer } from "./Navigation/User/LeftSideDrawer/LeftSideDrawer"
 
 export const GlobalWrapper = () => {
   const currentUser = useAuthContext()
 
   const comtentMap = new Map([
-    ['admin', ()=> <LefSideAdmin currentUser={currentUser}><AdminLayout/></LefSideAdmin>]
+    ['admin', ()=> <LefSideAdmin currentUser={currentUser}><AdminLayout/></LefSideAdmin>],
+    ['chife', ()=> <LeftSideDrawer currentUser={currentUser}><LeadLayout/></LeftSideDrawer>],
   ])
 
   const renderContent = () => {
