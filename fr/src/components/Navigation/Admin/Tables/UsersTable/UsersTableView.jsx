f609e87fc75d92c8c88f2c11163f1274956010c8
Scvotr <stasforeva23@mail.ru>
2024-03-06 07:20:53 +0300

    add name of modal by props

--- fr/src/components/Navigation/Admin/Tables/UsersTable/UsersTableView.jsx
+++ fr/src/components/Navigation/Admin/Tables/UsersTable/UsersTableView.jsx
@@ -87,7 +87,7 @@ export const UsersTableView = ({ actionType, users, reRender  }) => {
     <>
       <>
         {selectedUser && (
-          <ModalCustom isOpen={modalOpen} onClose={closeModal}>
+          <ModalCustom isOpen={modalOpen} onClose={closeModal} infoText='Редактировать'>
             <EditUserForm user={selectedUser} onTaskSubmit={closeModal} />
           </ModalCustom>
         )}
