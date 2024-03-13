9d922004a8bb19cd79706b9a33303322ca3f6875
Scvotr <stasforeva23@mail.ru>
2024-03-13 13:56:48 +0300

    add query for crur org struct

---
+++ bk/Database/queries/OrgStructure/adminQuery.js
@@ -0,0 +1,59 @@
+'use strict'
+const { executeDatabaseQueryAsync } = require("../../utils/executeDatabaseQuery/executeDatabaseQuery")
+
+const createNewDep = async (data) => {
+  const { dep_name } = data
+  const command = `INSERT INTO departments (name) VALUES (?)`
+  try {
+    await executeDatabaseQueryAsync(command, [ dep_name ], 'run')
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+const deleteDep = async (dep_id) => {
+  const command = 'DELETE FROM departments WHERE id = ?'
+  try {
+    await executeDatabaseQueryAsync(command, [ dep_id ], 'run')
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+const createNewSubDep = async (data) => {
+  const { department_id, subdep_name } = data
+  const command = `INSERT INTO subdepartments (department_id, name) VALUES (?, ?)`
+  try {
+    await executeDatabaseQueryAsync(command, [ department_id, subdep_name ], 'run')
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+const deleteSubDep = async (subDep_id) => {
+  const command = 'DELETE FROM subdepartments WHERE id = ?'
+  try {
+    await executeDatabaseQueryAsync(command, [ subDep_id ], 'run')
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+const createNewPosition = async (data) => {
+  const { position_name, department_id, subdepartment_id } = data;
+  const command = `INSERT INTO positions (name,  department_id, subdepartment_id) VALUES (?, ?, ?)`
+  try {
+    await executeDatabaseQueryAsync(command, [ position_name, department_id, subdepartment_id ], 'run')
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+
+module.exports = {
+  createNewDep,
+  deleteDep,
+  createNewSubDep,
+  deleteSubDep,
+  createNewPosition,
+}
