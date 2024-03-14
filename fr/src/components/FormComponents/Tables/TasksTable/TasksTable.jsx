37065eac7048dc9142c28c8daf1fdaf361089ed8
Scvotr <stasforeva23@mail.ru>
2024-03-14 16:24:34 +0300

    Обновление компонента TasksTable: добавлено обновление статуса задачи и обработка клика на ячейку

--- fr/src/components/FormComponents/Tables/TasksTable/TasksTable.jsx
+++ fr/src/components/FormComponents/Tables/TasksTable/TasksTable.jsx
@@ -1,7 +1,14 @@
 import { Box } from "@mui/material"
 import { DataGrid, ruRU } from "@mui/x-data-grid"
+import { useState } from "react"
+import { getDataFromEndpoint } from "../../../../utils/getDataFromEndpoint"
+import { useAuthContext } from "../../../../context/AuthProvider"
 
 export const TasksTable = ({ tasks }) => {
+  const currentUser = useAuthContext()
+  const [reqStatus, setReqStatus] = useState({ loading: true, error: null })
+  const [data, setData] = useState({})
+
   const columns = [
     { field: "task_id", headerName: "Task ID", description: "This column description", width: 100 },
     {
@@ -10,28 +17,44 @@ export const TasksTable = ({ tasks }) => {
       description: "This column description",
       width: 150,
       renderCell: params => (
-        <div style={{ fontWeight: params.value === "unread" ? "bold" : "normal", color: params.value === "unread" ? "red" : "black" }}>
-          {params.value}
-        </div>
+        <div style={{ fontWeight: params.value === "unread" ? "bold" : "normal", color: params.value === "unread" ? "red" : "black" }}>{params.value}</div>
       ),
     },
     { field: "created_on", headerName: "Создана", description: "This column description", width: 250 },
-  ];
+  ]
 
   const handleCellClick = (params, event) => {
-    console.log("Кликнута ячейка:", params.field, params.row.id, params.row, params.row.read_status);
-  };
+    console.log("Кликнута ячейка:", params.field, params.row.id, params.row, params.row.read_status)
+
+    if (params.row.read_status === "unread") {
+
+      setData({
+        task_id: params.row.task_id,
+        user_id: params.row.appoint_subdepartment_id,
+        read_status: "readed",
+      })
+
+      setReqStatus({ loading: true, error: null })
+      getDataFromEndpoint(currentUser.token, "/tasks/updateReadStatus", "POST", data, setReqStatus)
+        .then(data => {
+          setReqStatus({ loading: false, error: null })
+        })
+        .catch(error => {
+          setReqStatus({ loading: false, error: error.message })
+        })
+    }
+  }
 
   const sortedTasks = tasks.slice().sort((a, b) => {
-    if (a.read_status === "unread" && b.read_status === "read") {
-      return -1; // Переносим непрочитанные задачи в начало списка
-    } else if (a.read_status === "read" && b.read_status === "unread") {
-      return 1; // Переносим прочитанные задачи в конец списка
+    if (a.read_status === "unread" && b.read_status === "readed") {
+      return -1 // Переносим непрочитанные задачи в начало списка
+    } else if (a.read_status === "readed" && b.read_status === "unread") {
+      return 1 // Переносим прочитанные задачи в конец списка
     } else {
       // Если обе задачи имеют одинаковый статус или обе непрочитанные/прочитанные, сортируем по дате создания
-      return new Date(b.created_on) - new Date(a.created_on);
+      return new Date(b.created_on) - new Date(a.created_on)
     }
-  });
+  })
 
   return (
     <>
@@ -42,8 +65,7 @@ export const TasksTable = ({ tasks }) => {
           boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
           border: "1px solid #e0e0e0",
           borderRadius: "5px",
-        }}
-      >
+        }}>
         <DataGrid
           rowHeight={25}
           rows={sortedTasks.map(task => ({
@@ -56,5 +78,5 @@ export const TasksTable = ({ tasks }) => {
         />
       </Box>
     </>
-  );
-};
+  )
+}
