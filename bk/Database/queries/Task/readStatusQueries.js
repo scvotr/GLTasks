40aa92fd9f02d89ed7a88fb2645b1b1aa24cf2ef
Scvotr <stasforeva23@mail.ru>
2024-03-14 16:18:56 +0300

    добавил запрос на обновления статуса прочтено\не прочтено. Статус передается в качестве параметра. Подумать

--- bk/Database/queries/Task/readStatusQueries.js
+++ bk/Database/queries/Task/readStatusQueries.js
@@ -1,7 +1,7 @@
 'use strict'
 const { executeDatabaseQueryAsync } = require("../../utils/executeDatabaseQuery/executeDatabaseQuery")
 
-const addReadStatus = async(data) => {
+const addReadStatusQ = async(data) => {
   const {task_id, user_id, read_status} = data
   try {
     const command = `INSERT INTO task_read_status (task_id, user_id, read_status) VALUES (?, ?, ?)`
@@ -11,6 +11,17 @@ const addReadStatus = async(data) => {
   }
 }
 
+const updateReadStatusQ = async (data) => {
+  const { task_id, user_id, read_status } = data;
+  try {
+    const command = `UPDATE task_read_status SET read_status = ? WHERE task_id = ? AND user_id = ?`;
+    await executeDatabaseQueryAsync(command, [read_status, task_id, user_id]);
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных');
+  }
+};
+
 module.exports = {
-  addReadStatus,
+  addReadStatusQ,
+  updateReadStatusQ,
 }
