b2dcd1e1c9fe499062c69f6f2c8cbdc7a133512a
Scvotr <stasforeva23@mail.ru>
2024-03-14 11:14:05 +0300

    Добавлена функция readFileAsync для асинхронного чтения файлов

--- bk/utils/files/readFileAsync.js
+++ bk/utils/files/readFileAsync.js
@@ -1,2 +1,18 @@
 'use strict'
+const fs = require("fs");
 
+const readFileAsync = (file_path, encoding) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file_path, encoding, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+module.exports = {
+  readFileAsync,
+}
