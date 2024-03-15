7ad7c05e73d9d6c70d8516b400cd75c4fb60076f
Scvotr <stasforeva23@mail.ru>
2024-03-15 14:03:13 +0300

    Добален компонент для обработки событий по статусу задачи

---
+++ fr/src/components/FormComponents/Tables/RenderByAction/RenderByAction.jsx
@@ -0,0 +1,26 @@
+import { FullTaskInfo } from "../../Tasks/FullTaskInfo/FullTaskInfo"
+import { ToApprove } from "./ToApprove/ToApprove"
+
+export const RenderByAction = ({actionByStatus, task, onTaskSubmit}) => {
+  console.log(actionByStatus)
+
+  const actionTypes = new Map([
+    ["toApprove", () => <ToApprove task={task} onTaskSubmit={onTaskSubmit} />],
+    ["approved", () => console.log('>>>>>', actionByStatus)],
+    // ["underReview", () => <UnderReview task={task} onTaskSubmit={onTaskSubmit} />],
+    // ["setClose", () => <SetClose task={task} onTaskSubmit={onTaskSubmit} />],
+    ["viewOnly", () => <FullTaskInfo task={task} onTaskSubmit={onTaskSubmit} />],
+  ])
+
+  const renderByActionType = () => {
+    const render = actionTypes.get(actionByStatus)
+    // const render = actionTypes[actionType]
+    return render ? render() :  <>!!!</>
+  }
+
+  return (
+    <>
+      {renderByActionType()}
+    </>
+  )
+}
