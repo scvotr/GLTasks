5d8c8b2b2d99c0960abf6b7d9a9ae1f97c422ec7
Scvotr <stasforeva23@mail.ru>
2024-03-15 15:55:25 +0300

    Добавлен проверка роли при выборе компоннте от статуса задачи

--- fr/src/components/FormComponents/Tables/RenderByAction/RenderByAction.jsx
+++ fr/src/components/FormComponents/Tables/RenderByAction/RenderByAction.jsx
@@ -1,11 +1,13 @@
+import { useAuthContext } from "../../../../context/AuthProvider"
 import { FullTaskInfo } from "../../Tasks/FullTaskInfo/FullTaskInfo"
 import { ToApprove } from "./ToApprove/ToApprove"
 
 export const RenderByAction = ({actionByStatus, task, onTaskSubmit}) => {
+  const currentUser = useAuthContext()
   console.log(actionByStatus)
 
   const actionTypes = new Map([
-    ["toApprove", () => <ToApprove task={task} onTaskSubmit={onTaskSubmit} />],
+    ["toApprove", () => currentUser.role === 'chife' ? <ToApprove task={task} onTaskSubmit={onTaskSubmit} /> : <FullTaskInfo task={task} onTaskSubmit={onTaskSubmit} />],
     ["approved", () => console.log('>>>>>', actionByStatus)],
     // ["underReview", () => <UnderReview task={task} onTaskSubmit={onTaskSubmit} />],
     // ["setClose", () => <SetClose task={task} onTaskSubmit={onTaskSubmit} />],
