bcfcefbc6c570e48eceaddd6abb177008cd9e5e8
Scvotr <stasforeva23@mail.ru>
2024-03-15 13:56:15 +0300

    Добалена фильтрация по статусу и участникам нужно подумать или передалть полностью

--- fr/src/context/TasksProvider.jsx
+++ fr/src/context/TasksProvider.jsx
@@ -11,6 +11,8 @@ export const TasksProvider = ({currentUser, children}) => {
   const [reqStatus, setReqStatus] = useState({ loading: true, error: null })
   const [allTasks, setAllTasks] = useState([])
 
+  console.log(allTasks)
+
   const updateAllTasks = tasks => {
     setAllTasks(tasks)
   }
@@ -24,7 +26,18 @@ export const TasksProvider = ({currentUser, children}) => {
       case "need-all-Tasks":
           if(currentUser.login && currentUser.role === "chife") {
             fetchAllTasksBySubDep(currentUser.token)
-              .then(tasks => updateAllTasks(tasks))
+              .then(tasks => {
+                // Все задачи на согласование от отдела
+                const filteredTasks = tasks.filter(task => task.task_status === "toApprove" && task.appoint_subdepartment_id.toString() === currentUser.subDep.toString());
+                // Все согласованные задачи в отделе
+                const filteredTasks2 = tasks.filter(task => task.task_status === "approved" && task.appoint_subdepartment_id.toString() === currentUser.subDep.toString());
+                const combinedFilteredTasks = [...filteredTasks, ...filteredTasks2]
+                // Все согласованные задачи на отдел
+                const filteredTasks3 = tasks.filter(task => task.task_status === "approved" && task.responsible_subdepartment_id.toString() === currentUser.subDep.toString());
+                const test = [...combinedFilteredTasks, ...filteredTasks3]
+                updateAllTasks(test)
+                // updateAllTasks(tasks)
+              })
               .catch(error => console.error("Error fetching ALL tasks", error))
           }
         break
