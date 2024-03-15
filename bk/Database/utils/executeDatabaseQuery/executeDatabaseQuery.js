c2c06edf86e0a29e05f4aa8b7adbb45c0c2d8601
Scvotr <stasforeva23@mail.ru>
2024-03-15 15:53:27 +0300

    Временно отключен логер обращений к базе данных

--- bk/Database/utils/executeDatabaseQuery/executeDatabaseQuery.js
+++ bk/Database/utils/executeDatabaseQuery/executeDatabaseQuery.js
@@ -5,7 +5,7 @@ const logger = require("../../../utils/logger/logger");
 const { openConnection, closeConnection } = require("../activeConnections/activeConnectionsCount");
 
 const executeDatabaseQueryAsync = async (command, params = [], method = 'all') => {
-  logger.info(`Executing query: ${command} with params: ${JSON.stringify(params)}`);
+  // logger.info(`Executing query: ${command} with params: ${JSON.stringify(params)}`);
   openConnection(db);
   return new Promise((resolve, reject) => {
     db[method](command, params, (err, result) => {
