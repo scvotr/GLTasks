9c5bee3631785969da6f922f889233dace84b5a5
Scvotr <stasforeva23@mail.ru>
2024-03-18 16:54:44 +0300

    Добавлен компоннент для основной страницы редактирования задач

---
+++ fr/src/components/Navigation/Admin/Menu/Tasks/AllTasks.jsx
@@ -0,0 +1,51 @@
+import { AppBar, Box, Fab, Toolbar, Typography } from "@mui/material"
+import { AdminTasksTable } from "../../Tables/AdminTasksTable/AdminTasksTable"
+import { useCallback, useEffect, useState } from "react"
+import { useAuthContext } from "../../../../../context/AuthProvider"
+import { getDataFromEndpoint } from "../../../../../utils/getDataFromEndpoint"
+
+export const AllTasks = () => {
+  const currentUser = useAuthContext()
+  const [reqStatus, setReqStatus] = useState({ loading: true, error: null })
+  const [formKey, setFormKey] = useState(0)
+  const [dataFromEndpoint, setDataFromEndpoint] = useState([])
+
+  const fetchData = useCallback(async () => {
+    if (currentUser.login) {
+      try {
+        setReqStatus({ loading: true, error: null })
+        const data = await getDataFromEndpoint(currentUser.token, "/admin/getAllTasks", "POST", null, setReqStatus)
+        setDataFromEndpoint(data)
+        setReqStatus({ loading: false, error: null })
+      } catch (error) {
+        setReqStatus({ loading: false, error: error.message })
+      }
+    }
+  }, [currentUser, formKey])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  return (
+    <>
+      <Box>
+        <AppBar
+          position="static"
+          sx={{
+            mt: 2,
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+            border: "1px solid #e0e0e0",
+            borderRadius: "5px",
+          }}>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Все задачи:{" "}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <AdminTasksTable tasks={dataFromEndpoint} reRender={setFormKey} />
+    </>
+  )
+}
