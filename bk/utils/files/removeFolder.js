2dcc37df965a7b1baab014949496229148d27c38
Scvotr <stasforeva23@mail.ru>
2024-03-21 17:47:07 +0300

    обновлена функция. можно было и не трогать так как проблема была в другом (функция saveAndConver)'. добавлена проверка на доступ перед удалением, явно указываеем права для файла. Удалить!

--- bk/utils/files/removeFolder.js
+++ bk/utils/files/removeFolder.js
@@ -6,35 +6,43 @@ const path = require('path')
 const currentDirectory = process.cwd()
 const defaultPath = path.join(currentDirectory, 'uploads')
 
-const removeFolder = async (rootCustomFolder, folder) => {
+const removeFolder = async (folder, folderPath = defaultPath) => {
   try {
-    const fullPath = path.join(defaultPath, rootCustomFolder)
-    const fullFolderPath = path.join(fullPath, folder)
-    let files;
+    const fullFolderPath = path.join(defaultPath, folderPath, folder);
 
-    try {
-      files = await fs.promises.readdir(fullFolderPath);
-    } catch (err) {
-      if (err.code === 'ENOENT') {
-        return;
+    // Проверить существование каталога перед сканированием его содержимого
+    const folderExists = fs.existsSync(fullFolderPath);
+    if (!folderExists) {
+      console.log('Каталог не существует:', fullFolderPath);
+      return;
+    }
+
+    const files = await fs.promises.readdir(fullFolderPath);
+
+    for (const file of files) {
+      const filePath = path.join(fullFolderPath, file);
+      const stats = await fs.promises.lstat(filePath);
+
+      if (stats.isDirectory()) {
+        // Рекурсивно удалить подкаталог, если это каталог
+        await removeFolder(file, fullFolderPath);
       } else {
-        for (const file of files) {
-          const filePath = path.join(fullFolderPath, file)
-          const stats = await fs.promises.lstat(filePath)
-
-          if (stats.isDirectory()) {
-            await removeFolder(file, fullFolderPath)
-          } else {
-            await fs.promises.unlink(filePath)
-          }
-        }
-        await fs.promises.rmdir(fullFolderPath)
+        console.log('!!!!!!', filePath)
+        // Проверить доступ на запись перед удалением файла
+        await fs.promises.access(filePath, fs.constants.W_OK);
+        // Установить права доступа к файлу перед удалением
+        await fs.promises.chmod(filePath, 0o777); // Устанавливает полные права доступа
+        // Удалить файл, если это файл
+        await fs.promises.unlink(filePath);
       }
     }
+    // Удалить сам каталог после удаления всех файлов и подкаталогов
+    await fs.promises.rmdir(fullFolderPath);
   } catch (error) {
+    console.error(error);
     throw error;
   }
-}
+};
 
 module.exports = {
   removeFolder,
