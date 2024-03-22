bea6da04e2b592e605b3cf0e1fdd0f01a1857136
Scvotr <stasforeva23@mail.ru>
2024-03-22 07:32:17 +0300

    фильрация по условию вынесена в отдельную функцию

---
+++ fr/src/context/Tasks/useFilteredTasks.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect, useMemo } from "react"
+
+export const useFilteredTasks = (allTasks, currentUser, filterFunction) => {
+  const filteredTasks = useMemo(() => {
+    return currentUser ? allTasks.filter(filterFunction) : []
+  }, [allTasks, currentUser])
+
+  const [tasks, setTasks] = useState([])
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    setTasks(filteredTasks)
+    setCount(filteredTasks.length)
+  }, [filteredTasks])
+
+  return [tasks, count]
+}
