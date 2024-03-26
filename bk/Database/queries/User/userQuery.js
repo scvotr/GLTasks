3136ae4e380c237eda4bc1036e5004bb21311bd7
Scvotr <stasforeva23@mail.ru>
2024-03-26 10:18:16 +0300

    добавленны два запроса на поулчения пользователя по его ID и обновления данных профиля

---
+++ bk/Database/queries/User/userQuery.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const { executeDatabaseQueryAsync } = require("../../utils/executeDatabaseQuery/executeDatabaseQuery");
+
+const getUserByIdQ = async (userId) => {
+  try {
+    const command = `
+    SELECT 
+      users.id, 
+      users.name, 
+      users.email, 
+      users.role,
+      users.pin_code,
+      users.last_name,
+      users.first_name,
+      users.middle_name,
+      users.internal_phone,
+      users.external_phone,
+      users.office_number, 
+      users.created_on,
+      departments.id AS department_id,
+      departments.name AS department, 
+      subdepartments.id AS subdepartment_id,
+      subdepartments.name AS subdepartment,
+      positions.id AS position_id,
+      positions.name AS position
+    FROM 
+      users 
+      LEFT JOIN departments ON users.department_id = departments.id
+      LEFT JOIN subdepartments ON users.subdepartment_id = subdepartments.id
+      LEFT JOIN positions ON users.position_id = positions.id
+    WHERE users.id = ?;`;
+
+    return await executeDatabaseQueryAsync(command, [userId], 'get');
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных');
+  }
+}
+
+const editUserDataQ = async(data) => {
+  try {
+    console.log('>>>>>>', data)
+    const {id, last_name, first_name, middle_name, internal_phone, external_phone, office_number} = data
+    const command = `UPDATE users SET last_name = ?, first_name = ?, middle_name = ?, internal_phone = ?, external_phone = ?, office_number = ? WHERE id = ?`;
+    await executeDatabaseQueryAsync(command, [last_name, first_name, middle_name, internal_phone, external_phone, office_number, id], 'run')
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных')
+  }
+}
+
+module.exports = {
+  editUserDataQ,
+  getUserByIdQ,
+}
