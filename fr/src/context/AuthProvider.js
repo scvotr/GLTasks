24eaea4af1ef0febf4bd065a13a5343bfcdd1710
Scvotr <stasforeva23@mail.ru>
2024-03-26 16:58:33 +0300

    добавлены состояния когда профиль не заполнен и есл не назначен отдле. используются для доступа к созданию задач. с пустым профилем и не назначеным отделом создать задачу нельзя

--- fr/src/context/AuthProvider.js
+++ fr/src/context/AuthProvider.js
@@ -10,24 +10,30 @@ export const AuthProvider = ({children}) => {
   const [id, setId] = useState('')
   const [role, setRole] = useState('')
   const [roleRef, setRoleRef] = useState('')
+  const [loginName, setLoginName] = useState('')
   const [name, setName] = useState('')
   const [token, setToken] = useState('')
   const [dep, setDep] = useState('')
   const [subDep, setSubDep] = useState('')
   const [position, setPosition] = useState('')
   const [login, setLogin] = useState(false)
+  const [emptyProfile, setEmptyProfile] = useState('')
+  const [notDistributed, setNotDistributed] = useState('')
   
   useEffect(() => {
     const storedToken = localStorage.getItem('token')
     if (storedToken) {
       setToken(storedToken)
       setId(localStorage.getItem('userId'))
+      setId(localStorage.getItem('loginName'))
       setRole(localStorage.getItem('userRole'))
       setName(localStorage.getItem('userName'))
       setDep(localStorage.getItem('dep'))
       setSubDep(localStorage.getItem('subDep'))
       setPosition(localStorage.getItem('position'))
       setLogin(true);
+      setEmptyProfile(localStorage.getItem("emptyProfile"))
+      setNotDistributed(localStorage.getItem("notDistributed"))
     }
   }, []);
 
@@ -38,23 +44,29 @@ export const AuthProvider = ({children}) => {
 
   const handleServerResponse = useCallback((response) => {
     if (response) {
-      const { id, role, name, dep, subDep, position, token } = response
+      const { id, role, name, loginName, dep, subDep, position, token, emptyProfile, notDistributed} = response
       setId(id)
       setRole(role)
+      setLoginName(loginName)
       setName(name)
       setDep(dep)
       setSubDep(subDep)
       setPosition(position)
       setToken(token)
       setLogin(true)
+      setEmptyProfile(emptyProfile)
+      setNotDistributed(notDistributed)
       // Сохранение данных в localStorage
       localStorage.setItem("token", token)
+      localStorage.setItem("loginName", loginName)
       localStorage.setItem("userName", name)
       localStorage.setItem("userRole", role)
       localStorage.setItem("userId", id)
       localStorage.setItem("dep", dep)
       localStorage.setItem("subDep", subDep)
       localStorage.setItem("position", position)
+      localStorage.setItem("emptyProfile", emptyProfile)
+      localStorage.setItem("notDistributed", notDistributed)
     } else {
       setLogin(false);
     }
@@ -63,6 +75,7 @@ export const AuthProvider = ({children}) => {
   const logout = () => {
     setLogin(false);
     localStorage.removeItem('token');
+    localStorage.removeItem('loginName');
     localStorage.removeItem('userId');
     localStorage.removeItem('userRole');
     localStorage.removeItem('userRoleRef');
@@ -70,12 +83,15 @@ export const AuthProvider = ({children}) => {
     localStorage.removeItem('dep');
     localStorage.removeItem('subDep');
     localStorage.removeItem('position');
+    localStorage.removeItem('emptyProfile');
+    localStorage.removeItem('notDistributed');
   };
 
   return (
     <AuthContext.Provider
       value={{
         id,
+        loginName,
         name,
         role,
         roleRef,
@@ -84,9 +100,12 @@ export const AuthProvider = ({children}) => {
         dep,
         subDep,
         position,
+        emptyProfile,
+        notDistributed,
         handleServerResponse,
         updateToken,
         logout,
+        setEmptyProfile,
       }}
     >
       {children}
