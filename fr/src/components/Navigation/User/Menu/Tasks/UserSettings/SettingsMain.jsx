a291637dc9f71da26a787a6547d5a09bb70e19c0
Scvotr <stasforeva23@mail.ru>
2024-03-26 10:13:43 +0300

    добавлен компоненент для отображения данных из профиля пользователя

--- fr/src/components/Navigation/User/Menu/Tasks/UserSettings/SettingsMain.jsx
+++ fr/src/components/Navigation/User/Menu/Tasks/UserSettings/SettingsMain.jsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react"
+import { getDataFromEndpoint } from "../../../../../../utils/getDataFromEndpoint"
+import { useAuthContext } from "../../../../../../context/AuthProvider"
+import { Typography, Grid, Card, CardContent, Divider } from "@mui/material"
 
 export const SettingsMain = () => {
-  return(
+  const currentUser = useAuthContext()
+  const [reqStatus, setReqStatus] = useState({ loading: true, error: null })
+  const [userData, setUserData] = useState({})
+  const [errorPin, setErrorPin] = useState("")
+
+  useEffect(() => {
+    setReqStatus({ loading: true, error: null })
+    getDataFromEndpoint(currentUser.token, "/user/getUserById", "POST", null, setReqStatus)
+      .then(data => {
+        setUserData(data)
+        setReqStatus({ loading: false, error: null })
+      })
+      .catch(error => console.log(error))
+  }, [currentUser])
+  return (
     <>
-      SettingsMain
+      <Card>
+        <CardContent>
+          <Typography variant="body1">{userData.last_name}</Typography>
+          <Typography variant="body1">{userData.first_name}</Typography>
+          <Typography variant="body1">{userData.middle_name}</Typography>
+        </CardContent>
+        <CardContent>
+          <Typography variant="body1">{userData.department}</Typography>
+          <Typography variant="body1">{userData.subdepartment}</Typography>
+          <Typography variant="body1">{userData.position}</Typography>
+        </CardContent>
+        <CardContent>
+          <Typography variant="body1">
+            <strong>№ внутр. тел.:</strong> {userData.external_phone}
+          </Typography>
+          <Typography variant="body1">
+            <strong>№ тел./факс. :</strong> {userData.internal_phone}
+          </Typography>
+          <Typography variant="body1">
+            <strong>№ каб. :</strong> {userData.office_number}
+          </Typography>
+
+        </CardContent>
+      </Card>
     </>
   )
 }
