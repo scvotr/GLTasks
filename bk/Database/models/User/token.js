16eb8fecbd0535df35cfdc1156ed3286d0e696e3
Scvotr <stasforeva23@mail.ru>
2024-03-26 16:59:37 +0300

    удалены консоль логи

--- bk/Database/models/User/token.js
+++ bk/Database/models/User/token.js
@@ -21,10 +21,8 @@ const createTableTokens = async () => {
     if (rows['COUNT(*)'] === 0) { 
       const hashedPassword = await bcrypt.hash('0091', HASH_SALT)
       const objUser = { id: 999, login: 'admin', email: 'admin', password: hashedPassword, role: 'admin' }
-      console.log('createTableTokens >>>>>>>>>>>>', objUser)
       const token = jwt.sign(objUser, SECRET_KEY)
-      console.log('createTableTokens >>>>>>>>>>>>', token)
-      const command = `INSERT INTO tokens(user_id, token) VALUES (?, ?)`
+         const command = `INSERT INTO tokens(user_id, token) VALUES (?, ?)`
       await queryAsyncWraperParam(command, [objUser.id, token], `run`)
     }
   } catch (error) {
