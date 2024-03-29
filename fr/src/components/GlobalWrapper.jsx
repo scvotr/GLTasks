52f08938c4486d3a5e4fcfcb36a4abe135a80d50
Scvotr <stasforeva23@mail.ru>
2024-03-29 15:52:29 +0300

    Изменнеа логика добавления новых пользователей. Для всех новых пользователей свой кмопннет

--- fr/src/components/GlobalWrapper.jsx
+++ fr/src/components/GlobalWrapper.jsx
@@ -4,30 +4,24 @@ import { TasksProvider } from "../context/Tasks/TasksProvider"
 import { AdminLayout } from "./Layouts/AdminLayout/AdminLayout"
 import { DefaultLayoutMain } from "./Layouts/DefaultLayoutMain/DefaultLayoutMain"
 import { LeadLayout } from "./Layouts/LeadLayout/LeadLayout"
+import { NewUserLayout } from "./Layouts/NewUserLayout/NewUserLayout"
 import { UserLayout } from "./Layouts/UserLayout/UserLayout"
 import { LefSideAdmin } from "./Navigation/Admin/LefSideAdmin/LefSideAdmin"
 import { LeftSideDrawer } from "./Navigation/User/LeftSideDrawer/LeftSideDrawer"
 import { EditProfile } from "./Navigation/User/Menu/Tasks/UserSettings/EditProfile"
-import { NotDistributed } from "./Navigation/User/Menu/Tasks/UserSettings/NotDistributed"
 
 export const GlobalWrapper = () => {
   const currentUser = useAuthContext()
-  console.log(currentUser)
 
   const comtentMap = new Map([
     ["admin", () => (<LefSideAdmin currentUser={currentUser}><SocketProvider><AdminLayout /></SocketProvider></LefSideAdmin>),],
     ["chife", () => (<LeftSideDrawer currentUser={currentUser}><SocketProvider><LeadLayout /></SocketProvider></LeftSideDrawer>),],
     ["user", () => (<LeftSideDrawer currentUser={currentUser}><SocketProvider><UserLayout /></SocketProvider></LeftSideDrawer>),],
+    ["new", () => (<LeftSideDrawer currentUser={currentUser}><SocketProvider><NewUserLayout /></SocketProvider></LeftSideDrawer>),],
   ])
 
   const renderContent = () => {
     if (currentUser && currentUser.login) {
-      if(currentUser.emptyProfile) {
-        console.log(currentUser.emptyProfile && currentUser.role !== 'admin')
-        return  <LeftSideDrawer currentUser={currentUser}><SocketProvider><EditProfile text={'Для дальнейшей работы заполните профиль!'}/></SocketProvider></LeftSideDrawer>
-      } else if(currentUser.notDistributed && currentUser.role !== 'admin') {
-        return <LeftSideDrawer currentUser={currentUser}><SocketProvider><NotDistributed/></SocketProvider></LeftSideDrawer>
-      }
       const getContentByRole = comtentMap.get(currentUser.role)
       return getContentByRole ? getContentByRole() : <DefaultLayoutMain />
     } else {
