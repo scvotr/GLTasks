07ce946ee26fe0a2f17c19b24cd656e27f23e67a
Scvotr <stasforeva23@mail.ru>
2024-03-29 15:57:42 +0300

    Добавлено перенаправление на текущий компонент после обновления страницы в браузере

--- fr/src/components/Layouts/AdminLayout/AdminLayout.jsx
+++ fr/src/components/Layouts/AdminLayout/AdminLayout.jsx
@@ -1,9 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AdminLayout = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const savedRoute = localStorage.getItem("currentRoute");
+    if (savedRoute && savedRoute !== location.pathname) {
+      navigate(savedRoute);
+    }
+  }, [location, navigate]);
+
   return (
     <>
       <Outlet />
     </>
-  )
-}
+  );
+};
