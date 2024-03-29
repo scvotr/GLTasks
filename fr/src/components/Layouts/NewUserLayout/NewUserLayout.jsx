762a9b1a2d2b8c905ab33537a5e611d1fdfcc5d5
Scvotr <stasforeva23@mail.ru>
2024-03-29 15:53:32 +0300

    Добавлен слой для пользоваетелй с ролью new

---
+++ fr/src/components/Layouts/NewUserLayout/NewUserLayout.jsx
@@ -0,0 +1,63 @@
+import { Box } from "@mui/material"
+import { Outlet, useNavigate  } from "react-router-dom"
+import { useEffect, useState } from "react"
+import Snackbar from "@mui/material/Snackbar"
+import MuiAlert from "@mui/material/Alert"
+
+import { useAuthContext } from "../../../context/AuthProvider"
+import { useSocketContext } from "../../../context/SocketProvider"
+
+export const NewUserLayout = () => {
+  const currentUser = useAuthContext()
+  console.log('emptyProfile', currentUser.emptyProfile)
+  console.log('notDistributed', currentUser.notDistributed)
+
+  const [open, setOpen] = useState(false)
+  const [snackbarMessage, setSnackbarMessage] = useState("")
+  
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return
+    }
+    setOpen(false)
+  }
+
+  const socket = useSocketContext()
+  useEffect(() => {
+    socket.on("taskApproved", messageData => {
+      setSnackbarMessage(messageData.message)
+      setOpen(true)
+    })
+
+    return () => {
+      socket.off("yourRooms")
+      socket.off("taskApproved")
+      socket.disconnect()
+      window.removeEventListener("beforeunload", () => socket.disconnect())
+    }
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", () => {
+      socket.disconnect()
+    })
+    return () => {
+      window.removeEventListener("beforeunload", () => {
+        socket.disconnect()
+      })
+    }
+  }, [socket])
+
+  return (
+    <>
+      <Box>
+        <Snackbar open={open} autoHideDuration={16000} onClose={handleClose} anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+          <MuiAlert onClose={handleClose} severity="success" variant="filled" sx={{ width: "100%" }}>
+            {snackbarMessage}
+          </MuiAlert>
+        </Snackbar>
+      </Box>
+      <Outlet />
+    </>
+  )
+}
