d4f4fca711d032ad944d6cf6d6da9ee5bcd44040
Scvotr <stasforeva23@mail.ru>
2024-04-03 15:55:01 +0300

    отправка уведомления на почту вынесена в отдельную функцию. Созданы две функции дляотправки уведомление пользователю и руководителю

--- bk/controllers/Tasks/mailFor.js
+++ bk/controllers/Tasks/mailFor.js
@@ -1,99 +1,54 @@
 const nodemailer = require('nodemailer');
-const { getEmailQ } = require('../../Database/queries/User/userQuery');
+const {
+  getLeadEmailQ
+} = require('../../Database/queries/User/userQuery');
 
-// Функция для отправки электронной почты
-const sendEmail = async(user_id) => {
+require("dotenv").config()
+const MAIL_USER = process.env.MAIL_USER
+const MAIL_PASS = process.env.MAIL_PASS
 
-  console.log('sendEmail', user_id)
+const sendEmail = async (recipientEmail, text) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'mail.nic.ru',
+      port: 587,
+      secure: false,
+      auth: {
+        user: MAIL_USER,
+        pass: MAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: true,
+      },
+    });
 
-  const email = await getEmailQ(user_id)
-  console.log(email)
+    const mailOptions = {
+      from: `"${text}" <${MAIL_USER}>`,
+      to: recipientEmail,
+      subject: text,
+      text: text,
+    };
 
-  // try {
-  //   const transporter = nodemailer.createTransport({
-  //     host: 'smtp.mail.ru',
-  //     port: 465,
-  //     secure: true,
-  //     auth: {
-  //       user: 'stasforeva23@mail.ru',
-  //       pass: '36abwseVfHpjJRPHDnZ3',
-  //     },
-  //     tls: {
-  //       rejectUnauthorized: false,
-  //     },
-  //   });
-
-  //   const mailOptions = {
-  //     from: '"The Idea project" <stasforeva23@mail.ru>',
-  //     to: 'it.ae@geliopax.ru', // Уберите угловые скобки из адреса получателя
-  //     subject: 'Send message from project',
-  //     text: 'Hello',
-  //   };
-
-  //   const info = await transporter.sendMail(mailOptions);
-  //   console.log('Email sent:', info.response);
-  // } catch (error) {
-  //   console.error('Error occurred while sending email:', error);
-  // }
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.response);
+  } catch (error) {
+    console.error('Error occurred while sending email:', error);
+  }
 }
 
-// Вызов функции для отправки письма
-sendEmail();
-
-module.exports ={
-  sendEmail,
+const sendEmailToLead = async (subdepartment_id, text) => {
+  const email = await getLeadEmailQ(subdepartment_id);
+  console.log('sendEmailToLead', email[0].email_for_notify);
+  await sendEmail(email[0].email_for_notify, text);
 }
-// !--------------------------------------
-// const transpoter = nodemailer.createTransport({
-//     host: 'smtp.mail.ru',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//         user: 'stasforeva23@mail.ru',
-//         pass: '36abwseVfHpjJRPHDnZ3',
-//     },
-//     tls: {
-//         rejectUnauthorized: false,
-//     },
-// });
 
-// const mailOptions = {
-//   from: '"The Idea project" <stasforeva23@mail.ru>',
-//     to: '<it.ae@geliopax.ru>',
-//     subject: 'Send message from project',
-//     text: 'Hello',
-// };
+const sendEmailToUser = async (user_id, text) => {
+  const email = await getUserEmailQ(user_id);
+  console.log(email);
+  await sendEmail(email[0].email_for_notify, text);
+}
 
-// transpoter.sendMail(mailOptions, (err, info) => {
-//     console.log(err, info);
-// });
-
-
-
-// const transporter = nodemailer.createTransport({
-//   // настройки для вашего почтового сервера
-//   host: 'smtp.mail.ru',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: 'mail@mail.ru',
-//     pass: 'pass',
-//   },
-//   tls: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// const sendMail = async (recipient, subject, message) => {
-//   try {
-//     await transporter.sendMail({
-//       from: '<mail.mailru>', // ваш адрес электронной почты
-//       to: recipient,
-//       subject: subject,
-//       text: message,
-//     });
-//     console.log('Письмо успешно отправлено');
-//   } catch (error) {
-//     console.error('Ошибка отправки письма:', error);
-//   }
-// };
+module.exports = {
+  sendEmailToUser,
+  sendEmailToLead,
+}
