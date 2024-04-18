79ab458110aa2c6d6ae3f32dc17069cadc958b91
Scvotr <stasforeva23@mail.ru>
2024-04-18 07:55:58 +0300

    добавленны запрос на получения всех задачь по id департамента

--- bk/Database/queries/Task/taskQueries.js
+++ bk/Database/queries/Task/taskQueries.js
@@ -164,6 +164,69 @@ const removeTaskQ = async (data, taskFolderName) => {
   }
 }
 
+const getAllTasksByDepQ = async (dep_id) => {
+  const command = `
+    SELECT
+      t.id, 
+      t.task_id,
+      t.task_descript,
+      t.task_priority,
+      t.task_status,
+      t.deadline,
+      t.appoint_user_id,
+      t.created_on,
+      t.approved_on,          -- Дата согласования задачи
+      t.reject_on,            -- Дата отклонения задачи
+      t.confirmation_on,      -- Дата запрос на подтверждение задачи
+      t.closed_on,            -- Дата закрытия задачи
+      t.setResponseSubDep_on, -- Дата назначения отдела
+      t.setResponseUser_on,   -- Дата назначения пользователя
+      appoint_user_f_name.first_name AS appoint_user_name,
+      appoint_user_l_name.last_name AS appoint_user_last_name,
+      appoint_user_m_name.middle_name AS appoint_user_middle_name,
+      t.appoint_department_id, 
+      appoint_departments.name AS appoint_department_name,
+      t.appoint_subdepartment_id,
+      appoint_subdepartments.name AS appoint_subdepartment_name,
+      t.responsible_user_id,
+      responsible_f_user.first_name AS responsible_user_name,
+      responsible_l_user.last_name AS responsible_user_last_name,
+      responsible_m_user.middle_name AS responsible_user_middle_name,
+      t.responsible_department_id, 
+      responsible_departments.name AS responsible_department_name,
+      t.responsible_subdepartment_id,
+      responsible_subdepartments.name AS responsible_subdepartment_name,
+      t.responsible_position_id,
+      responsible_position.name AS responsible_position_name,
+      GROUP_CONCAT(f.file_name, '|') AS file_names,
+      trs.read_status AS read_status
+    FROM tasks t
+      LEFT JOIN users AS appoint_user_f_name ON t.appoint_user_id = appoint_user_f_name.id
+      LEFT JOIN users AS appoint_user_l_name ON t.appoint_user_id = appoint_user_l_name.id
+      LEFT JOIN users AS appoint_user_m_name ON t.appoint_user_id = appoint_user_m_name.id
+      LEFT JOIN departments AS appoint_departments ON t.appoint_department_id = appoint_departments.id
+      LEFT JOIN subdepartments AS appoint_subdepartments ON t.appoint_subdepartment_id = appoint_subdepartments.id
+      LEFT JOIN users AS responsible_f_user ON t.responsible_user_id = responsible_f_user.id
+      LEFT JOIN users AS responsible_l_user ON t.responsible_user_id = responsible_l_user.id
+      LEFT JOIN users AS responsible_m_user ON t.responsible_user_id = responsible_m_user.id
+      LEFT JOIN departments AS responsible_departments ON t.responsible_department_id = responsible_departments.id
+      LEFT JOIN subdepartments AS responsible_subdepartments ON t.responsible_subdepartment_id = responsible_subdepartments.id
+      LEFT JOIN positions AS responsible_position ON t.responsible_position_id = responsible_position.id
+      LEFT JOIN task_files f ON t.task_id = f.task_id
+      LEFT JOIN (
+        SELECT task_id, user_id, read_status
+        FROM task_read_status
+        WHERE user_id = ?
+      ) trs ON t.task_id = trs.task_id
+    WHERE ? IN (t.appoint_department_id, t.responsible_department_id)
+    GROUP BY t.task_id `;
+  try {
+    const taskFiles = await executeDatabaseQueryAsync(command, [dep_id, dep_id])
+    return await getThumbnailFiles(taskFiles, 'tasks')
+  } catch (error) {
+    throw new Error('Ошибка запроса к базе данных');
+  }
+}
 const getAllTasksBySubDepQ = async (subDep_id) => {
   const command = `
     SELECT
@@ -338,6 +401,7 @@ module.exports = {
   updateTaskDescription,
   updateTaskStatusQ,
   removeTaskQ,
+  getAllTasksByDepQ,
   getAllTasksBySubDepQ,
   getAllUserTasksQ,
   updateTaskByEventrQ,
