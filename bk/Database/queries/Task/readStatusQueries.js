375a0e4feaf84dac198f0a008cac8510727d8095
Scvotr <stasforeva23@mail.ru>
2024-04-18 07:56:49 +0300

    убрана отладочная информация

--- bk/Database/queries/Task/readStatusQueries.js
+++ bk/Database/queries/Task/readStatusQueries.js
@@ -3,6 +3,7 @@ const { executeDatabaseQueryAsync } = require("../../utils/executeDatabaseQuery/
 
 const addReadStatusQ = async(data) => {
   const {task_id, user_id, read_status} = data
+  // console.log('addReadStatusQ', task_id, user_id, read_status)
   try {
     const command = `INSERT INTO task_read_status (task_id, user_id, read_status) VALUES (?, ?, ?)`
     await executeDatabaseQueryAsync(command, [task_id, user_id, read_status])
@@ -13,6 +14,7 @@ const addReadStatusQ = async(data) => {
 
 const updateReadStatusQ = async (data) => {
   const { task_id, user_id, read_status } = data;
+  // console.log('updateReadStatusQ', task_id, user_id, read_status)
   try {
     const command = `UPDATE task_read_status SET read_status = ? WHERE task_id = ? AND user_id = ?`;
     await executeDatabaseQueryAsync(command, [read_status, task_id, user_id]);
