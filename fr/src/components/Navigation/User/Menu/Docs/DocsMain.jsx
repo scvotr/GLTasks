c80420980f654dbe1bd19e7344e70876ed72eea0
Scvotr <stasforeva23@mail.ru>
2024-04-27 08:14:23 +0300

    app bar вынесен в отдельный помпонент

--- fr/src/components/Navigation/User/Menu/Docs/DocsMain.jsx
+++ fr/src/components/Navigation/User/Menu/Docs/DocsMain.jsx
@@ -9,6 +9,7 @@ import { AddNewDocForm } from "../../../../FormComponents/Docs/AddNewDocForm"
 import { DocToPDF } from "../../../../FormComponents/Docs/DocToPDF"
 import { UseAccordionView } from "../../../../FormComponents/Accordion/UseAccordionView"
 import { PDFbyPinFromDoc } from "../../../../FormComponents/Docs/PDFbyPinFromDoc"
+import { AppBarForPage } from "../componenst/AppBarForPage/AppBarForPage"
 
 const Item = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -42,43 +43,25 @@ export const DocsMain = () => {
       <>
         <ModalCustom isOpen={modalOpen} onClose={closeModal} infoText="Новая задача"></ModalCustom>
       </>
-      <AppBar
-        position="static"
-        sx={{
-          mt: 2,
-          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-          border: "1px solid #e0e0e0",
-          borderRadius: "5px",
-        }}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Документы:
-          </Typography>
-          <Fab color="secondary" aria-label="add" onClick={openModal}>
-            <AddIcon />
-          </Fab>
-        </Toolbar>
-      </AppBar>
+      <AppBarForPage title="Документы:" />
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <Item>
-            <UseAccordionView 
-              headerText='Заявление дни в счет отпуска:'
-              bodyText='Прошу предоставить мне дд.мм.гггг c чч:мм до чч:мм в счет отпуска.'>
+            <UseAccordionView headerText="Заявление дни в счет отпуска:" bodyText="Прошу предоставить мне дд.мм.гггг c чч:мм до чч:мм в счет отпуска.">
               <AddNewDocForm />
             </UseAccordionView>
           </Item>
         </Grid>
         <Grid item xs={6}>
           <Item>
-            <UseAccordionView headerText='Создать PDF:'>
+            <UseAccordionView headerText="Создать PDF:">
               <DocToPDF />
             </UseAccordionView>
           </Item>
         </Grid>
         <Grid item xs={6}>
           <Item>
-            <UseAccordionView headerText='Создать PDF через PINCODE:'>
+            <UseAccordionView headerText="Создать PDF через PINCODE:">
               <PDFbyPinFromDoc />
             </UseAccordionView>
           </Item>
