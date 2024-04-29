d5eb0ab05d7fe3cad733c86b0b2e43ddb683ed43
scvotr <71510141+scvotr@users.noreply.github.com>
2024-04-29 10:42:24 +0300

    Изменне тип позиционирования

--- fr/src/components/FullScreenDialog/FullScreenDialog.jsx
+++ fr/src/components/FullScreenDialog/FullScreenDialog.jsx
@@ -10,7 +10,7 @@ import IconButton from "@mui/material/IconButton"
 import Typography from "@mui/material/Typography"
 import CloseIcon from "@mui/icons-material/Close"
 import Slide from "@mui/material/Slide"
-import { useState, forwardRef } from "react"
+import { forwardRef } from "react"
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />
@@ -20,24 +20,49 @@ export const FullScreenDialog = ({ isOpen, onClose, children, infoText }) => {
   return (
     <>
       <Dialog fullScreen open={isOpen} onClose={onClose} TransitionComponent={Transition}>
-        <AppBar sx={{ position: "relative" }}>
-          <Toolbar>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              {infoText}
-            </Typography>
-            {/* <Button autoFocus color="inherit" onClick={onClose}>
-              save
-            </Button> */}
-            <IconButton edge="end" color="inherit" onClick={onClose} aria-label="close">
-              <CloseIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-          {children}
+          <AppBar position="sticky" > 
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                {infoText}
+              </Typography>
+              <IconButton color="inherit" onClick={onClose} aria-label="close">
+                <CloseIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        {children}
       </Dialog>
     </>
   )
 }
+
+// !V
+// const Transition = forwardRef(function Transition(props, ref) {
+//   return <Slide direction="up" ref={ref} {...props} />
+// })
+
+// export const FullScreenDialog = ({ isOpen, onClose, children, infoText }) => {
+//   return (
+//     <>
+//       <Dialog fullScreen open={isOpen} onClose={onClose} TransitionComponent={Transition}>
+//         <AppBar sx={{ position: "relative" }}>
+//           <Toolbar>
+//             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+//               {infoText}
+//             </Typography>
+//             {/* <Button autoFocus color="inherit" onClick={onClose}>
+//               save
+//             </Button> */}
+//             <IconButton edge="end" color="inherit" onClick={onClose} aria-label="close">
+//               <CloseIcon />
+//             </IconButton>
+//           </Toolbar>
+//         </AppBar>
+//           {children}
+//       </Dialog>
+//     </>
+//   )
+// }
 
 
         {/* <List>
