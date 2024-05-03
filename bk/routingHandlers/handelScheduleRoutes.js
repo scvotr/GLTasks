1ebd7e79604adcc0c16ed19aab2f8f0820020680
scvotr <71510141+scvotr@users.noreply.github.com>
2024-05-03 05:35:50 +0300

    add routes handeler for schedules

---
+++ bk/routingHandlers/handelScheduleRoutes.js
@@ -0,0 +1,39 @@
+const ScheduleController = require("../controllers/Schedule/ScheduleController");
+const { handleDefaultRoute } = require("../routingHandlers/handleDefaultRoute");
+const { protectRouteTkPl } = require("../utils/protectRouteTkPl");
+
+
+const routeHandlers = {
+  "/schedule/addSchedules": ScheduleController.authDecodeUserData,
+  "/schedule/getAllSchedulesByUserId": ScheduleController.getAllSchedulesByUserId,
+  "/schedule/getAllSchedulesBySubDepId": ScheduleController.getAllSchedulesBySubDepId,
+}
+
+const handelScheduleRoutes = async(req, res) => {
+  const { url, method } = req;
+  try {
+    if (url.startsWith("/schedule")) {
+      if (method === "POST") {
+        const routeHandler = routeHandlers[url];
+        if (routeHandler) {
+          await protectRouteTkPl(routeHandler)(req, res);
+        } else {
+          handleDefaultRoute(req, res);
+        }
+      } else {
+        handleDefaultRoute(req, res);
+      }
+    } else {
+      handleDefaultRoute(req, res);
+    }
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "handelScheduleRoutes - ERROR" }));
+  }
+}
+
+module.exports = {
+  handelScheduleRoutes
+};
