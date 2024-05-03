3c969ecc3b2366843eca2444e3d7623c357c4191
Scvotr <stasforeva23@mail.ru>
2024-05-03 16:14:23 +0300

    add sub form for mutual verify

---
+++ fr/src/components/Navigation/User/Menu/MutualVerification/MutualVerificationDocs.jsx
@@ -0,0 +1,7 @@
+import { AppBarForPage } from "../componenst/AppBarForPage/AppBarForPage"
+
+export const MutualVerificationDocs = () => {
+  return (
+    <AppBarForPage title='Взаимопроверка - Документы'/>
+  )
+}
