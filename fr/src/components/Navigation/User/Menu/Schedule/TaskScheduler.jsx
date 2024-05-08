a8f56ccdbdfe1028aca7769310fe795bc72dffb9
Scvotr <stasforeva23@mail.ru>
2024-05-08 08:27:29 +0300

    rm files

--- fr/src/components/Navigation/User/Menu/Schedule/TaskScheduler.jsx
+++
@@ -1,9 +0,0 @@
-import { AppBarForPage } from "../componenst/AppBarForPage/AppBarForPage"
-
-export const TaskScheduler = () => {
-  return (
-    <>
-      <AppBarForPage title="Планировщик задач: " />
-    </>
-  )
-}
