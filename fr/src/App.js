ff8eb8c07ba694da42979d0b2383d8f1c286f622
Scvotr <stasforeva23@mail.ru>
2024-05-08 13:07:48 +0300

    test thems styles for app

--- fr/src/App.js
+++ fr/src/App.js
@@ -1,23 +1,40 @@
-import { createTheme, ThemeProvider, CssBaseline, Container, Box } from '@mui/material'
+import { createTheme, responsiveFontSizes, ThemeProvider, CssBaseline, Container, Box } from '@mui/material'
 import { GlobalWrapper } from './components/GlobalWrapper'
 import { SocketProvider } from './context/SocketProvider'
 import { SnackbarProvider } from './context/SnackbarProvider'
 
-const defaultTheme = createTheme()
+let defaultTheme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: '#3f51b5',
+  //     main: '#007bff',
+  //   },
+  //   secondary: {
+  //     main: '#f50057',
+  //     main: '#6c757d',
+  //   },
+  // },
+  // typography: {
+    // fontFamily: 'Roboto, sans-serif',
+    // fontSize: 16,
+  // },
+})
+
+defaultTheme = responsiveFontSizes(defaultTheme)
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-        <CssBaseline />
-        {/* <SocketProvider> */}
-          <Container maxWidth={false}>
-            <Box>
-              <SnackbarProvider>
-                <GlobalWrapper />
-              </SnackbarProvider>
-            </Box>
-          </Container>
-        {/* </SocketProvider> */}
+      <CssBaseline />
+      {/* <SocketProvider> */}
+      <Container maxWidth={false}>
+        <Box>
+          <SnackbarProvider>
+            <GlobalWrapper />
+          </SnackbarProvider>
+        </Box>
+      </Container>
+      {/* </SocketProvider> */}
     </ThemeProvider>
   )
 }
