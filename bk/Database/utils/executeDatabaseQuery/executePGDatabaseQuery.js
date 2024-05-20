9b000aa9f03e95f4e4580900a790b8cbf934ffa2
Scvotr <stasforeva23@mail.ru>
2024-05-20 16:57:10 +0300

    edit function for postgres exec

--- bk/Database/utils/executeDatabaseQuery/executePGDatabaseQuery.js
+++ bk/Database/utils/executeDatabaseQuery/executePGDatabaseQuery.js
@@ -1,31 +1,51 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg')
+const logger = require('../../../utils/logger/logger')
+require('dotenv').config()
 
-// Параметры подключения к вашей базе данных PostgreSQL
+// Создаем новый пул соединений к вашей удаленной базе данных PostgreSQL
 const pool = new Pool({
-  user: 'your_username',
-  host: 'your_host',
-  database: 'your_database',
-  password: 'your_password',
-  port: 5432, // порт вашей базы данных PostgreSQL
-});
+  user: process.env.POSTGRES_USER || 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  database: process.env.POSTGRES_DB || 'gltest',
+  password: process.env.POSTGRES_PASSWORD || '0000',
+  port: process.env.POSTGRES_PORT || 5432,
+})
 
 const executePGDatabaseQuery = async (command, params = [], method = 'query') => {
-  const client = await pool.connect(); // Получаем клиента из пула соединений
-
   try {
-    const result = await client[method](command, params);
-    return result.rows; // Возвращаем результат запроса
-  } finally {
-    client.release(); // Возвращаем клиента в пул после выполнения запроса
+    const client = await pool.connect() // Получаем клиента из пула соединений
+    try {
+      logger.info(`Connection successfully opened. Query is: ${command}`)
+      const result = await client[method](command, params)
+      return result.rows
+    } finally {
+      client.release()
+    }
+  } catch (error) {
+    logger.error(`Error executing query: ${error}`);
+    throw error
   }
-};
-
+}
 
 module.exports = {
-  executePGDatabaseQuery
+  executePGDatabaseQuery,
 }
 
 // usege
 
 // const result = await executeDatabaseQueryAsync('SELECT * FROM your_table');
 // console.log(result); // Вывод результата запроса
+
+// query
+// const result = await executePGDatabaseQuery('SELECT * FROM users');
+
+// insert
+// const result = await executePGDatabaseQuery('INSERT INTO users (name, email) VALUES ($1, $2)', ['John', 'john@example.com'], 'insert');
+
+// update
+// const result = await executePGDatabaseQuery('UPDATE users SET name = $1 WHERE id = $2', ['Jane', 123], 'update');
+
+// delete
+// const result = await executePGDatabaseQuery('DELETE FROM users WHERE id = $1', [123], 'delete');
+
+
