cd958f9f420b1fd54f531f084d6a4672dac70da8
Scvotr <stasforeva23@mail.ru>
2024-06-03 15:33:44 +0300

    add new props for expanded form

--- fr/src/components/FormComponents/Accordion/UseAccordionView.jsx
+++ fr/src/components/FormComponents/Accordion/UseAccordionView.jsx
@@ -7,10 +7,10 @@ import Button from "@mui/material/Button"
 import Typography from "@mui/material/Typography"
 import Box from "@mui/material/Box"
 
-export const UseAccordionView = ({ headerText, bodyText, children }) => {
+export const UseAccordionView = ({ headerText, bodyText, expanded, children }) => {
   return (
     <>
-      <Accordion>
+      <Accordion expanded={expanded}>
         <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel3-content" id="panel3-header">
           <Box sx={{ textAlign: "center", width: "100%" }}>
             <Typography variant="h8" fontWeight="bold">
