8152c09341017db6ede9efab0b433eff7f3f0c15
scvotr <you@example.com>
2024-06-20 09:45:48 +0300

    hsts

--- fr/src/utils/remoteHosts.js
+++ fr/src/utils/remoteHosts.js
@@ -4,8 +4,8 @@
 //export const HOST_ADDR = 'http://localhost:3050'
 //export const HOST_SOCKET = 'http://localhost:3050'
 
-export const HOST_ADDR = 'http://192.168.8.109:3030'
-export const HOST_SOCKET = 'http://192.168.8.109:3030'
+export const HOST_ADDR = 'http://192.168.8.109:3050'
+export const HOST_SOCKET = 'http://192.168.8.109:3050'
 
 // export const HOST_ADDR = 'http://192.168.50.77:3070'
 // export const HOST_SOCKET = 'http://192.168.50.77:3070'
