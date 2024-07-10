83841df6921caab791fc139265352dbffee37bce
Scvotr <stasforeva23@mail.ru>
2024-07-10 16:16:04 +0300

    add cmp for sub dep tasks

---
+++ fr/src/components/Navigation/User/Menu/Tasks/AllSubDepTasks.jsx
@@ -0,0 +1,35 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material"
+import { useTaskContext } from "../../../../../context/Tasks/TasksProvider"
+import { useEffect, useState } from "react"
+import { TasksTable } from "../../../../FormComponents/Tables/TasksTable/TasksTable"
+
+export const AllSubDepTasks = () => {
+  const { allLeadTasks, allSubDepTasks } = useTaskContext()
+  const [formKey, setFormKey] = useState(0)
+
+  console.log(allLeadTasks)
+
+  useEffect(() => {}, [formKey])
+
+  return (
+    <>
+      <Box>
+        <AppBar
+          position="static"
+          sx={{
+            mt: 2,
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+            border: "1px solid #e0e0e0",
+            borderRadius: "5px",
+          }}>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Закрытые задачи:
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <TasksTable tasks={allSubDepTasks || []} reRender={setFormKey} />
+      </Box>
+    </>
+  )
+}
