eb46756c40bf245d51fa830ce3477d02d2553981
Scvotr <stasforeva23@mail.ru>
2024-07-10 16:38:17 +0300

    add button to create new task

--- fr/src/components/Navigation/User/Menu/Tasks/LeadTasks.jsx
+++ fr/src/components/Navigation/User/Menu/Tasks/LeadTasks.jsx
@@ -2,34 +2,36 @@ import { AppBar, Box, Toolbar, Typography } from "@mui/material"
 import { useTaskContext } from "../../../../../context/Tasks/TasksProvider"
 import { useEffect, useState } from "react"
 import { TasksTable } from "../../../../FormComponents/Tables/TasksTable/TasksTable"
+import { TaskForm } from "./TaskForm/TaskForm"
+import { ModalCustom } from "../../../../ModalCustom/ModalCustom"
+import { AppBarForPage } from "../components/AppBarForPage/AppBarForPage"
 
 export const LeadTasks = () => {
-  const { closedAndReadedTasks, allTasksNoClosed, allLeadTasks, allSubDepTasks } = useTaskContext()
+  const { allLeadTasks, notifyEvent } = useTaskContext()
   const [formKey, setFormKey] = useState(0)
+  const [modalOpen, setModalOpen] = useState(false)
 
   console.log(allLeadTasks)
 
-  useEffect(() => {}, [formKey])
+  useEffect(() => {
+    notifyEvent("need-all-Tasks")
+  }, [formKey])
+
+  const openModal = user => {
+    setModalOpen(true)
+  }
+  const closeModal = () => {
+    setModalOpen(false)
+    setFormKey(prevKey => prevKey + 1)
+  }
 
   return (
     <>
-      <Box>
-        <AppBar
-          position="static"
-          sx={{
-            mt: 2,
-            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-            border: "1px solid #e0e0e0",
-            borderRadius: "5px",
-          }}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Закрытые задачи:
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <TasksTable tasks={allLeadTasks || []} reRender={setFormKey} />
-      </Box>
+      <ModalCustom isOpen={modalOpen} onClose={closeModal} infoText="Новая задача">
+        <TaskForm onTaskSubmit={closeModal} />
+      </ModalCustom>
+      <AppBarForPage title="Мои Задачи: " openModal={openModal} />
+      <TasksTable tasks={allLeadTasks || []} reRender={setFormKey} />
     </>
   )
 }
