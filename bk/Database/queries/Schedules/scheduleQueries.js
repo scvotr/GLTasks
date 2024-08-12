b26ccfef02572054486879529039ce1d17398a98
Scvotr <stasforeva23@mail.ru>
2024-08-12 15:28:57 +0300

    add query for upadate schedule

--- bk/Database/queries/Schedules/scheduleQueries.js
+++ bk/Database/queries/Schedules/scheduleQueries.js
@@ -31,47 +31,105 @@ const createSchedulesQ = async data => {
     }
   } catch (error) {
     console.error('Error - createSchedules:', error)
-    throw error;
+    throw error
   }
 }
 
-const getAllSchedulesByUserIdQ = async (user_id) => {
+const updateScheduleQ = async data => {
+  const command = `
+  UPDATE schedules
+  SET
+    assign_user_id = ?, 
+    schedule_status = ?, 
+    schedule_type = ?, 
+    schedule_title = ?, 
+    schedule_description = ?, 
+    schedule_comment = ?, 
+    deadline_time = ?, 
+    estimated_time = ?, 
+    schedule_priority = ?, 
+    schedule_priority_rate = ?, 
+    appoint_user_id = ?, 
+    appoint_department_id = ?, 
+    appoint_subdepartment_id = ?, 
+    appoint_position_id = ?
+  WHERE
+    schedule_id = ?
+`
+  const command2 = `
+  UPDATE schedules
+  SET
+    schedule_status = ?
+  WHERE
+    schedule_id = ?
+`
+  const command3 = `
+  UPDATE schedules
+  SET
+    schedule_description = ?
+  WHERE
+    schedule_id = ?
+`
+
+  try {
+    const payload = JSON.parse(data.payLoad)
+    console.log('dddddddd', payload)
+    
+    if(payload.schedule_description) {
+      await executeDatabaseQueryAsync(command3, [
+        payload.schedule_description,
+        payload.schedule_id, // schedule_id используется в условии WHERE
+      ])
+    } else {
+      await executeDatabaseQueryAsync(command2, [
+        payload.schedule_status,
+        payload.schedule_id, // schedule_id используется в условии WHERE
+      ])
+    }  
+  } catch (error) {
+    console.error('Error - updateSchedules:', error)
+    throw error
+  }
+}
+
+const getAllSchedulesByUserIdQ = async user_id => {
   const command = `
     SELECT schedules.*
     FROM schedules
     LEFT JOIN users AS assign_user ON schedules.assign_user_id = assign_user.id
     LEFT JOIN users AS appoint_user ON schedules.appoint_user_id = appoint_user.id
     WHERE assign_user.id = ?
-  `;
+  `
 
   try {
-    const result = await executeDatabaseQueryAsync(command, [user_id]);
-    return result; // Возвращаем результат выполнения запроса
+    const result = await executeDatabaseQueryAsync(command, [user_id])
+    return result // Возвращаем результат выполнения запроса
   } catch (error) {
-    console.error('Error fetching schedules by user ID:', error);
-    throw error; // Пробрасываем ошибку для дальнейшей обработки
+    console.error('Error fetching schedules by user ID:', error)
+    throw error // Пробрасываем ошибку для дальнейшей обработки
   }
 }
 
-const removeScheduleByIdQ = async (schedule_id) => {
+const removeScheduleByIdQ = async schedule_id => {
   const command = `
     DELETE FROM schedules
     WHERE schedule_id = ?
-  `;
+  `
 
   //! REMOVE COMMENTS
 
   try {
-    const result = await executeDatabaseQueryAsync(command, [schedule_id]);
+    const result = await executeDatabaseQueryAsync(command, [schedule_id])
     return result
   } catch (error) {
-    console.error('Error fetching remove schedules by schedule ID:', error);
+    console.error('Error fetching remove schedules by schedule ID:', error)
     throw error
   }
-};
+}
 
 module.exports = {
   createSchedulesQ,
+  updateScheduleQ,
   getAllSchedulesByUserIdQ,
   removeScheduleByIdQ,
 }
