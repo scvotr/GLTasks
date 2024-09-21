5eca93f4addc41b8d61b10839f9e3fff970356eb
scvotr <71510141+scvotr@users.noreply.github.com>
2024-09-21 12:37:32 +0300

    Renamed component PrivateRoutes to PrivateRoutesCheck

--- fr/src/routes/routes.js
+++ fr/src/routes/routes.js
@@ -60,123 +60,123 @@ export const routes = createBrowserRouter([
       },
       {
         path: '/admin/users',
-        element: <PrivateRoutes component={Users} roles={["admin"]}/>,
+        element: <PrivateRoutesCheck component={Users} roles={['admin']} />,
       },
       {
         path: '/admin/users/new',
-        element: <PrivateRoutes component={NewUsers} roles={["admin"]}/>,
+        element: <PrivateRoutesCheck component={NewUsers} roles={['admin']} />,
       },
       {
         path: '/admin/struct',
-        element: <PrivateRoutes component={Struct} roles={["admin"]}/>,
+        element: <PrivateRoutesCheck component={Struct} roles={['admin']} />,
       },
       {
         path: '/admin/struct/new',
-        element: <PrivateRoutes component={NewStruct} roles={["admin"]}/>,
+        element: <PrivateRoutesCheck component={NewStruct} roles={['admin']} />,
       },
       {
         path: '/admin/tasks',
-        element: <PrivateRoutes component={Tasks} roles={["admin"]}/>,
+        element: <PrivateRoutesCheck component={Tasks} roles={['admin']} />,
       },
       {
         path: '/admin/tasks/all',
-        element: <PrivateRoutes component={AllTasks} roles={["admin"]}/>,
+        element: <PrivateRoutesCheck component={AllTasks} roles={['admin']} />,
       },
       {
         path: '/main2',
-        element: <PrivateRoutes component={InfoNews} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={InfoNews} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/manualTasks',
-        element: <PrivateRoutes component={TasksManualCmp} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={TasksManualCmp} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/tasks',
-        element: <PrivateRoutes component={TasksMain} roles={["chife", "user"]}/>,
+        element: <PrivateRoutesCheck component={TasksMain} roles={['chife', 'user']} />,
       },
       {
         path: '/general/tasks',
-        element: <PrivateRoutes component={GeneralTasksMain} roles={["general"]}/>,
+        element: <PrivateRoutesCheck component={GeneralTasksMain} roles={['general']} />,
       },
       {
         path: '/tasks/leadTasks',
-        element: <PrivateRoutes component={LeadTasks} roles={["chife"]}/>,
+        element: <PrivateRoutesCheck component={LeadTasks} roles={['chife']} />,
       },
       {
         path: '/tasks/allTasksFromSubDep',
-        element: <PrivateRoutes component={AllTasksFromSubDep} roles={["chife"]}/>,
+        element: <PrivateRoutesCheck component={AllTasksFromSubDep} roles={['chife']} />,
       },
       {
         path: '/tasks/allTasksToSubDep',
-        element: <PrivateRoutes component={AllTasksToSubDep} roles={["chife"]}/>,
+        element: <PrivateRoutesCheck component={AllTasksToSubDep} roles={['chife']} />,
       },
       {
         path: '/tasks/helpDesk',
-        element: <PrivateRoutes component={HelpDeskTasks} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={HelpDeskTasks} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/tasks/closedTask',
-        element: <PrivateRoutes component={ClosedTask} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={ClosedTask} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/tasks/AnalyticsChart',
-        element: <PrivateRoutes component={AnalyticsChart} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={AnalyticsChart} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/settings',
-        element: <PrivateRoutes component={SettingsMain} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={SettingsMain} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/settings/profile',
-        element: <PrivateRoutes component={EditProfile} roles={["chife", "user", "new", "general"]}/>,
+        element: <PrivateRoutesCheck component={EditProfile} roles={['chife', 'user', 'new', 'general']} />,
       },
       {
         path: '/settings/changePasPin',
-        element: <PrivateRoutes component={ChangePasPin} roles={["chife", "user", "new", "general"]}/>,
+        element: <PrivateRoutesCheck component={ChangePasPin} roles={['chife', 'user', 'new', 'general']} />,
       },
       {
         path: '/settings/emptyProfile',
-        element: <PrivateRoutes component={EmptyProfile} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={EmptyProfile} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/docs',
-        element: <PrivateRoutes component={DocsMain} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={DocsMain} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/docs/docsArchive',
-        element: <PrivateRoutes component={DocsAtchive} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={DocsAtchive} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/docs/ordinance',
-        element: <PrivateRoutes component={DocsOrdinance} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={DocsOrdinance} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/docs/ordinance',
-        element: <PrivateRoutes component={DocsOrdinance} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={DocsOrdinance} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/schedule',
-        element: <PrivateRoutes component={SchedulMain} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={SchedulMain} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/schedule/schedulesArchive',
-        element: <PrivateRoutes component={SchedulsArchive} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={SchedulsArchive} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/schedule/taskScheduler',
-        element: <PrivateRoutes component={UsersSchedules} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={UsersSchedules} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/mutualVerification',
-        element: <PrivateRoutes component={MutualVerificationMain} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={MutualVerificationMain} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/mutualVerification/docs',
-        element: <PrivateRoutes component={MutualVerificationDocs} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={MutualVerificationDocs} roles={['chife', 'user', 'general']} />,
       },
       {
         path: '/mutualVerification/archive',
-        element: <PrivateRoutes component={MutualVerificationArchive} roles={["chife", "user", "general"]}/>,
+        element: <PrivateRoutesCheck component={MutualVerificationArchive} roles={['chife', 'user', 'general']} />,
       },
     ]
   }
