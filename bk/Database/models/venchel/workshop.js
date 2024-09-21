25bd5facb878efa448addfe6767009e0a29a0320
scvotr <71510141+scvotr@users.noreply.github.com>
2024-09-21 13:25:15 +0300

    update crud for workshops !!!need renaming or chenge for class

--- bk/Database/models/venchel/workshop.js
+++ bk/Database/models/venchel/workshop.js
@@ -24,3 +24,46 @@ const createTableWorkshops = async () => {
 module.exports = {
   createTableWorkshops
 }
+
+
+// const { executeDatabaseQueryAsync, queryAsyncWraper } = require("../../utils/executeDatabaseQuery/executeDatabaseQuery");
+
+// const createTables = async () => {
+//   try {
+//     // Создаем таблицу workshops
+//     await executeDatabaseQueryAsync(
+//       `CREATE TABLE IF NOT EXISTS workshops (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         department_id INTEGER NOT NULL,
+//         name TEXT NOT NULL,
+//         FOREIGN KEY (department_id) REFERENCES departments(id)
+//       )`, []);
+
+//     // Создаем таблицу sectors
+//     await executeDatabaseQueryAsync(
+//       `CREATE TABLE IF NOT EXISTS sectors (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         workshop_id INTEGER NOT NULL,
+//         name TEXT NOT NULL,
+//         FOREIGN KEY (workshop_id) REFERENCES workshops(id)
+//       )`, []);
+
+//     // Проверяем наличие записей в таблице workshops
+//     const rowsWorkshops = await queryAsyncWraper('SELECT COUNT(*) FROM workshops', 'get');
+//     if (rowsWorkshops['COUNT(*)'] === 0) { // Если записей нет, вставляем начальные значения
+//       await queryAsyncWraper("INSERT INTO workshops (department_id, name) VALUES (3, 'Элеватор №1'), (3, 'Элеватор №2'), (4, 'Элеватор №1'), (4, 'Элеватор №2'), (4, 'Склад №5'), (4, 'Склады')", 'run');
+//     }
+
+//     // Проверяем наличие записей в таблице sectors
+//     const rowsSectors = await queryAsyncWraper('SELECT COUNT(*) FROM sectors', 'get');
+//     if (rowsSectors['COUNT(*)'] === 0) { // Если записей нет, вставляем начальные значения
+//       await queryAsyncWraper("INSERT INTO sectors (workshop_id, name) VALUES (1, 'Ларь'), (1, 'Рабочая башня'), (2, 'Нижн.гал. с\\к№11'), (2, 'Нижн.гал. с\\к№12')", 'run');
+//     }
+//   } catch (error) {
+//     console.error('createTables ERROR: ', error);
+//   }
+// };
+
+// module.exports = {
+//   createTables
+// };
