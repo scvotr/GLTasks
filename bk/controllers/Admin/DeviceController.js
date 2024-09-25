fd9ad0b8fbd21e38283a1a5b617167811c6d4cfd
Scvotr <stasforeva23@mail.ru>
2024-09-25 06:04:30 +0300

    change to import responce function

--- bk/controllers/Admin/DeviceController.js
+++ bk/controllers/Admin/DeviceController.js
@@ -1,24 +1,8 @@
 'use strict'
 
-const DeviceCRUD = require('../../Database/queries/Devices/MotorsMotorPowerCRUD')
+const DeviceCRUD = require('../../Database/queries/Devices/DeviceCRUD')
 const DeviceTypesCRUD = require('../../Database/queries/Devices/DeviceTypes/DeviceTypesCRUD')
-
-const sendResponseWithData = (res, data) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.write(JSON.stringify(data))
-  res.end()
-}
-
-const handleError = (res, error) => {
-  console.log('handleError', error)
-  res.statusCode = 500
-  res.end(
-    JSON.stringify({
-      error: error,
-    })
-  )
-}
-
+const { sendResponseWithData, handleError } = require('../../utils/response/responseUtils')
 class DeviceController {
   async createBucketElevator(req, res) {
     try {
