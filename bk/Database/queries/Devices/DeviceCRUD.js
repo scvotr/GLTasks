723c6530d9c5044cb1f34a17bd78f180e53825e2
Scvotr <stasforeva23@mail.ru>
2024-09-26 07:01:34 +0300

    update check for tech num by workshop

--- bk/Database/queries/Devices/DeviceCRUD.js
+++ bk/Database/queries/Devices/DeviceCRUD.js
@@ -20,11 +20,36 @@ class DeviceCRUD {
     const { device_id, tech_num, type_id, workshop_id, department_id } = data
     const QRC = await generateQRCodeToURL(device_id)
     try {
+      // Проверка на существование имени, если передано название поля
+      if (tech_num) {
+        const checkCommand = `SELECT COUNT(*) AS count FROM devices WHERE tech_num = ?
+          AND workshop_id = ?`
+        const checkResult = await executeDatabaseQueryAsync(checkCommand, [tech_num, workshop_id])
+
+        if (checkResult[0].count > 0) {
+          throw new Error(`Запись с таким ${tech_num} уже существует`) // Выбрасываем ошибку, если запись уже существует
+        }
+      }
+
       const command = `
       INSERT INTO devices (device_id, tech_num, qr_code, type_id, workshop_id, department_id)
       VALUES (?, ?, ?, ?, ?, ?)
     `
       await executeDatabaseQueryAsync(command, [device_id, tech_num, QRC, type_id, workshop_id, department_id])
+    } catch (error) {
+      console.error('Error creating new device:', error)
+      throw new Error('Ошибка запроса к базе данных')
+    }
+  }
+
+  async appendMotorQ(data) {
+    const { device_id, motor_config_id } = data
+    try {
+      const command = `
+      INSERT INTO motors (device_id, motor_config_id)
+      VALUES (?, ?)
+    `
+      await executeDatabaseQueryAsync(command, [device_id, motor_config_id])
     } catch (error) {
       console.error('Error creating new device:', error)
       throw new Error('Ошибка запроса к базе данных')
@@ -73,34 +98,34 @@ class DeviceCRUD {
   async getAllBucketElevatorsWithDetailsQ() {
     try {
       const command = `
-      SELECT 
-        be.device_id,
-        be.height,
-        d.tech_num,
-        d.qr_code,
-        w.name AS workshop_name,  
-        dep.name AS department_name,
-        mt.name AS type_name,
-        bb.brand_name AS beltBrands_name,
-        be.belt_installation_date,
-        be.belt_length,
-        bub.brand_name AS bucketBrand_name,
-        be.bucket_quantity,
-        be.bucket_installation_date,
-        gb.brand_name AS gearboxBrand_name,
-        be.gearbox_installation_date,
-        db.brand_name AS driveBeltsBrand_name,
-        be.driveBelt_quantity,
-        be.driveBelt_installation_date
-      FROM bucketElevators be
-      JOIN devices d ON be.device_id = d.device_id
-      LEFT JOIN workshops w ON d.workshop_id = w.id
-      LEFT JOIN departments dep ON d.department_id = dep.id
-      LEFT JOIN devicesTypes mt ON d.type_id = mt.id
-      LEFT JOIN beltBrands bb ON be.belt_brand_id = bb.id
-      LEFT JOIN bucketBrands bub ON be.bucket_brand_id = bub.id
-      LEFT JOIN gearboxBrands gb ON be.gearbox_brand_id = gb.id
-      LEFT JOIN driveBelts db ON be.driveBelt_brand_id = db.id
+        SELECT 
+          be.device_id,
+          be.height,
+          d.tech_num,
+          d.qr_code,
+          w.name AS workshop_name,  
+          dep.name AS department_name,
+          mt.name AS type_name,
+          bb.brand_name AS beltBrands_name,
+          be.belt_installation_date,
+          be.belt_length,
+          bub.brand_name AS bucketBrand_name,
+          be.bucket_quantity,
+          be.bucket_installation_date,
+          gb.brand_name AS gearboxBrand_name,
+          be.gearbox_installation_date,
+          db.brand_name AS driveBeltsBrand_name,
+          be.driveBelt_quantity,
+          be.driveBelt_installation_date
+        FROM bucketElevators be
+        JOIN devices d ON be.device_id = d.device_id
+        LEFT JOIN workshops w ON d.workshop_id = w.id
+        LEFT JOIN departments dep ON d.department_id = dep.id
+        LEFT JOIN devicesTypes mt ON d.type_id = mt.id
+        LEFT JOIN beltBrands bb ON be.belt_brand_id = bb.id
+        LEFT JOIN bucketBrands bub ON be.bucket_brand_id = bub.id
+        LEFT JOIN gearboxBrands gb ON be.gearbox_brand_id = gb.id
+        LEFT JOIN driveBelts db ON be.driveBelt_brand_id = db.id
       `
       const results = await executeDatabaseQueryAsync(command)
       return results
@@ -116,6 +141,50 @@ class DeviceCRUD {
     } catch (error) {
       console.error('Ошибка при удалении типа устройства:', error)
       throw new Error('Ошибка запроса к базе данных при удалении типа устройства')
+    }
+  }
+  // !-------------------
+  async getAllDevicesQ() {
+    try {
+      const command = `
+        SELECT 
+          d.device_id, 
+          d.devices_installation_date, 
+          d.tech_num, 
+          d.qr_code, 
+          dt.name AS type_name,
+          w.name AS workshop_name,  
+          dep.name AS department_name,
+          p.name AS power_value
+        FROM 
+          devices d
+        LEFT JOIN 
+          devicesTypes dt ON d.type_id = dt.id
+        LEFT JOIN 
+          workshops w ON d.workshop_id = w.id
+        LEFT JOIN 
+          departments dep ON d.department_id = dep.id
+        LEFT JOIN 
+            motors m ON m.device_id = d.device_id
+        LEFT JOIN 
+          motors_config mc ON mc.motor_config_id = m.motor_config_id  
+        LEFT JOIN 
+          motorPowerRangeT p ON mc.power_id = p.id  
+      `
+      const results = await executeDatabaseQueryAsync(command)
+      return results
+    } catch (error) {
+      console.error('Error fetching bucket elevators with details:', error)
+      throw new Error('Ошибка запроса к базе данных')
+    }
+  }
+
+  async deleteDeviceQ(data) {
+    try {
+      const command = `DELETE FROM devices WHERE device_id = ?`
+      await executeDatabaseQueryAsync(command, [data])
+    } catch (error) {
+      throw new Error('Ошибка запроса к базе данных')
     }
   }
 }
@@ -163,3 +232,36 @@ module.exports = new DeviceCRUD()
 // LEFT JOIN gearboxBrands gb ON be.gearbox_brand_id = gb.id
 // LEFT JOIN driveBelts db ON be.driveBelt_brand_id = db.id
 // LEFT JOIN beltBrands lb ON be.belt_brand_id = lb.id
+
+
+// const command = `
+// SELECT 
+//     d.*, 
+//     m.*, 
+//     mc.*, 
+//     mb.name AS brand_name, 
+//     mm.name AS model_name,
+//     dt.name AS type_name,
+//     w.name AS workshop_name,  
+//     dep.name AS department_name,
+//     be.height
+// FROM 
+//     devices d
+// LEFT JOIN 
+//     motors m ON m.device_id = d.device_id
+// LEFT JOIN 
+//     motors_config mc ON mc.motor_config_id = m.motor_config_id
+// LEFT JOIN 
+//     motor_brands mb ON mc.brand_id = mb.id
+// LEFT JOIN 
+//     motor_models mm ON mc.model_id = mm.id
+// LEFT JOIN 
+//   devicesTypes dt ON d.type_id = dt.id
+// LEFT JOIN 
+//   workshops w ON d.workshop_id = w.id
+// LEFT JOIN 
+//   departments dep ON d.department_id = dep.id
+// LEFT JOIN 
+//   bucketElevators be ON d.device_id = be.device_id
+
+// `
