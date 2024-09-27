ceb8d1e384688e4c766bec621c14a41022fb273a
Scvotr <stasforeva23@mail.ru>
2024-09-27 06:13:50 +0300

    add qrcode info & print. add spacing for action button

--- fr/src/components/FormComponents/Device/DeviceInfoView/DeviceInfoView.jsx
+++ fr/src/components/FormComponents/Device/DeviceInfoView/DeviceInfoView.jsx
@@ -6,6 +6,7 @@ import TabList from "@mui/lab/TabList"
 import TabPanel from "@mui/lab/TabPanel"
 import { MotorInfoView } from "../MotorInfoView/MotorInfoView"
 import { TypeInfoView } from "../TypeInfoView/TypeInfoView"
+import { QRCodePrinter } from "../../../Navigation/Admin/Menu/Machines/QRCode/QRCodePrinter"
 
 export const DeviceInfoView = ({ device }) => {
   const [value, setValue] = useState("1")
@@ -27,6 +28,7 @@ export const DeviceInfoView = ({ device }) => {
                 <TableCell align="center">Тип</TableCell>
                 <TableCell align="center">Тех. номер</TableCell>
                 <TableCell align="center">кВт</TableCell>
+                <TableCell align="center">qr_code</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -35,6 +37,16 @@ export const DeviceInfoView = ({ device }) => {
               <TableCell align="center">{device.type_name || "---"}</TableCell>
               <TableCell align="center">{device.tech_num || "---"}</TableCell>
               <TableCell align="center">{device.power_value || "---"}</TableCell>
+              <TableCell align="center">
+                {device.qr_code && typeof device.qr_code === "string" && device.qr_code.startsWith("data:image/png;base64,") ? (
+                  <>
+                    <img src={device.qr_code} alt="QR Code" style={{ maxWidth: "100%", height: "auto" }} />
+                    {/* <QRCodePrinter qrCodeData={device.qr_code} /> */}
+                  </>
+                ) : (
+                  <p>QR Code not available</p>
+                )}
+              </TableCell>
             </TableBody>
           </Table>
         </TableContainer>
