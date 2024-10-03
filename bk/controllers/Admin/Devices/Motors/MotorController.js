d240c6e3377ff57956330097593de0b01d30db45
Scvotr <stasforeva23@mail.ru>
2024-10-03 15:16:54 +0300

    add two methods for create and read motors

--- bk/controllers/Admin/Devices/Motors/MotorController.js
+++ bk/controllers/Admin/Devices/Motors/MotorController.js
@@ -1,10 +1,32 @@
 'use strict'
 
+const MotorCRUD = require('../../../../Database/queries/Devices/Motors/MotorCRUD')
+const MotorConfigCRUD = require('../../../../Database/queries/Devices/Motors/MotorConfigCRUD')
+const { createMotorQ } = require('../../../../Database/queries/Devices/Motors/MotorConfigCRUD')
+const { handleError, sendResponseWithData } = require('../../../../utils/response/responseUtils')
 const BaseMotorController = require('./BaseMotorController')
 
 class MotorController extends BaseMotorController {
   constructor() {
     super('motors')
+  }
+  async createMotor(req, res) {
+    try {
+      const authDecodeUserData = req.user
+      const data = JSON.parse(authDecodeUserData.payLoad)
+      await MotorCRUD.createMotorQ(data)
+      sendResponseWithData(res, 'createMotor-ok')
+    } catch (error) {
+      handleError(res, 'Error: createMotor')
+    }
+  }
+  async readAllMotors(req, res) {
+    try {
+      const data = await MotorCRUD.getAllMotorsQ()
+      sendResponseWithData(res, data)
+    } catch (error) {
+      handleError(res, 'Error: readAllDevices')
+    }
   }
 }
 
