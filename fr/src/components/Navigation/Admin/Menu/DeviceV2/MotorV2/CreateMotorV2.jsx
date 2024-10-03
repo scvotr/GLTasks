ad0f7ab2b7adafde99b8c49ce141be94f8455239
Scvotr <stasforeva23@mail.ru>
2024-10-03 15:26:49 +0300

    test

---
+++ fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/CreateMotorV2.jsx
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useState } from "react"
+import { ModalCustom } from "../../../../../ModalCustom/ModalCustom"
+import { AppBar, Box, Button, Fab, Toolbar, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Stack } from "@mui/material"
+import AddIcon from "@mui/icons-material/Add"
+import { NavLink } from "react-router-dom"
+import { useAuthContext } from "../../../../../../context/AuthProvider"
+import { fetchData } from "../../../../../../utils/fetchData"
+import { Loader } from "../../../../../FormComponents/Loader/Loader"
+import { getDataFromEndpoint } from "../../../../../../utils/getDataFromEndpoint"
+
+export const CreateMotorV2 = () => {
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const closeModal = () => {
+    setModalOpen(false)
+  }
+
+  return (
+    <>
+      <ModalCustom isOpen={modalOpen} onClose={closeModal} infoText="Добавить двигатель"></ModalCustom>
+      <Box>
+        <AppBar
+          position="static"
+          sx={{
+            mt: 2,
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+            border: "1px solid #e0e0e0",
+            borderRadius: "5px",
+          }}>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Добавить двигатель:
+            </Typography>
+            {/* <NavigationButtons sections={sections} /> */}
+            <Fab color="secondary" aria-label="add" onClick={() => setModalOpen(true)}>
+              <AddIcon />
+            </Fab>
+            <NavLink to="/admin/devicesV2/all" style={{ textDecoration: "none", color: "inherit", margin: "0 8px" }}>
+              <Button color="inherit">назад</Button>
+            </NavLink>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  )
+}
