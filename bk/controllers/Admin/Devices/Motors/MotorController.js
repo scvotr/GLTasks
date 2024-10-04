985712aa4aa9d4b43b99f1fbe7f8081afde229d4
Scvotr <stasforeva23@mail.ru>
2024-10-04 17:03:14 +0300

    add methods for delete & update. return error text if error

--- bk/controllers/Admin/Devices/Motors/MotorController.js
+++ bk/controllers/Admin/Devices/Motors/MotorController.js
@@ -17,13 +17,34 @@ class MotorController extends BaseMotorController {
       await MotorCRUD.createMotorQ(data)
       sendResponseWithData(res, 'createMotor-ok')
     } catch (error) {
-      handleError(res, 'Error: createMotor')
+      handleError(res, `Error: createMotor - ${error.message}`) // Передаем сообщение об ошибке клиенту
+    }
+  }
+  async updateMotor(req, res) {
+    try {
+      const authDecodeUserData = req.user
+      const data = JSON.parse(authDecodeUserData.payLoad)
+      await MotorCRUD.updateMotorQ(data)
+      sendResponseWithData(res, 'updateMotor-ok')
+    } catch (error) {
+      handleError(res, `Error: updateMotor - ${error.message}`) // Передаем сообщение об ошибке клиенту
     }
   }
   async readAllMotors(req, res) {
     try {
       const data = await MotorCRUD.getAllMotorsQ()
       sendResponseWithData(res, data)
+    } catch (error) {
+      handleError(res, 'Error: readAllDevices')
+    }
+  }
+  async deleteMotors(req, res) {
+    try {
+      const authDecodeUserData = req.user
+      const motor_id = JSON.parse(authDecodeUserData.payLoad)
+      console.log(motor_id)
+      await MotorCRUD.deleteMotorsQ(motor_id)
+      sendResponseWithData(res, `Успешно удален ${motor_id}`)
     } catch (error) {
       handleError(res, 'Error: readAllDevices')
     }
