2af2fc214cee073ddaca1a963cf862bd06d0f116
Scvotr <stasforeva23@mail.ru>
2024-10-04 17:04:30 +0300

    add key down handler

--- fr/src/components/FormComponents/ConfirmationDialog/ConfirmationDialog.jsx
+++ fr/src/components/FormComponents/ConfirmationDialog/ConfirmationDialog.jsx
@@ -2,38 +2,45 @@ import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, D
 
 export const ConfirmationDialog = ({ open, onClose, onConfirm, title, message }) => {
   const handleConfirmDelete = () => {
-    onConfirm();
-    onClose();
-  };
+    onConfirm()
+    onClose()
+  }
+
+  const handleKeyDown = e => {
+    if (e.key === "Enter") {
+      e.preventDefault() // Предотвращаем стандартное поведение
+      handleConfirmDelete() // Вызываем подтверждение
+    }
+  }
 
   return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      PaperProps={{ style: { minWidth: "300px" } }}
-    >
+    <Dialog open={open} onClose={onClose} onKeyDown={handleKeyDown} PaperProps={{ style: { minWidth: "300px" } }}>
       <Box style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
         <DialogTitle>{title}</DialogTitle>
         <DialogContent>
           <DialogContentText>{message}</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={onClose} variant="contained" >Отмена</Button>
+          <Button onClick={onClose} variant="contained">
+            Отмена
+          </Button>
           <Button onClick={handleConfirmDelete} variant="contained" color="error">
             Подтвердить
           </Button>
         </DialogActions>
       </Box>
     </Dialog>
-  );
-};
+  )
+}
 
 // usege
 
-{/* <ConfirmationDialog
+{
+  /* <ConfirmationDialog
   open={openDialog}
   onClose={() => setOpenDialog(false)}
   onConfirm={handleConfirmDelete}
   title="Подтвердите удаление задачи"
   message="Вы уверены, что хотите удалить эту задачу?"
-/> */}
+/> */
+}
