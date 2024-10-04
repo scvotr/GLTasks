4dfa2212c845a0739c179a49406c0f50c6f4e9c4
Scvotr <stasforeva23@mail.ru>
2024-10-04 17:06:12 +0300

    to future thung about

--- fr/src/context/SnackbarProvider.jsx
+++ fr/src/context/SnackbarProvider.jsx
@@ -26,3 +26,55 @@ export const SnackbarProvider = ({ children }) => {
     {children}
    </SnackbarContext.Provider>)
 }
+
+// export const CreateMotorV2 = () => {
+//   const [modalOpen, setModalOpen] = useState(false);
+//   const { openSnackbar } = useSnackbar(); // Получаем функцию openSnackbar
+
+//   const closeModal = () => {
+//     setModalOpen(false);
+//   };
+
+//   const handleCreateMotor = () => {
+//     // Логика создания двигателя
+//     // После успешного создания двигателя
+//     openSnackbar("Двигатель успешно добавлен!"); // Открываем Snackbar с сообщением
+//     closeModal(); // Закрываем модальное окно
+//   };
+
+// import React, { createContext, useContext, useState } from "react";
+// import { CustomSnackbar } from "./CustomSnackbar"; // Убедитесь, что путь правильный
+
+// const SnackbarContext = createContext();
+
+// export const useSnackbar = () => {
+//   return useContext(SnackbarContext);
+// };
+
+// export const SnackbarProvider = ({ children }) => {
+//   const [snackbarOpen, setSnackbarOpen] = useState(false);
+//   const [snackbarMessage, setSnackbarMessage] = useState("");
+//   const [snackbarSeverity, setSnackbarSeverity] = useState("success"); // Уровень серьезности по умолчанию
+
+//   const openSnackbar = (message, severity = "success") => {
+//     setSnackbarMessage(message);
+//     setSnackbarSeverity(severity);
+//     setSnackbarOpen(true);
+//   };
+
+//   const closeSnackbar = () => {
+//     setSnackbarOpen(false);
+//   };
+
+//   return (
+//     <SnackbarContext.Provider value={{ openSnackbar, snackbarOpen, snackbarMessage, snackbarSeverity }}>
+//       {children}
+//       <CustomSnackbar 
+//         open={snackbarOpen} 
+//         message={snackbarMessage} 
+//         severity={snackbarSeverity} 
+//         onClose={closeSnackbar} 
+//       />
+//     </SnackbarContext.Provider>
+//   );
+// };
