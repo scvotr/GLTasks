5d82e0b7043f17c1fe8a0ec8704d91e782f664d0
Scvotr <stasforeva23@mail.ru>
2024-10-10 09:05:03 +0300

    add check for datatime format

--- fr/src/utils/formatDate.js
+++ fr/src/utils/formatDate.js
@@ -1,32 +1,37 @@
-export const formatDate = (dateString) => {
+export const formatDate = dateString => {
   // Функция для форматирования даты
   // const options = { year: "numeric", month: "long", day: "numeric" };
   // const options = { year: "numeric", month: "numeric", day: "numeric" };
-  const options = { day: "numeric", month: "numeric", year: "numeric" };
-  return new Date(dateString).toLocaleDateString(undefined, options);
-};
+  const options = { day: 'numeric', month: 'numeric', year: 'numeric' }
+  return new Date(dateString).toLocaleDateString(undefined, options)
+}
 
 export const formatDateV2 = (dateString, includeTime = false) => {
   // Опции для форматирования даты
-  const dateOptions = { day: "numeric", month: "numeric", year: "numeric" };
-  
+  const dateOptions = { day: 'numeric', month: 'numeric', year: 'numeric' }
+
   // Опции для форматирования времени
-  const timeOptions = { hour: "numeric", minute: "numeric", second: "numeric" };
+  const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' }
 
   // Создаем объект даты
-  const date = new Date(dateString);
+  const date = new Date(dateString)
+
+  // Проверяем, является ли дата корректной
+  if (isNaN(date.getTime())) {
+    return 'Данных нет' // Или любое другое сообщение об ошибке
+  }
 
   // Форматируем дату
-  const formattedDate = date.toLocaleDateString(undefined, dateOptions);
+  const formattedDate = date.toLocaleDateString(undefined, dateOptions)
 
   // Если нужно включить время, форматируем и его
   if (includeTime) {
-    const formattedTime = date.toLocaleTimeString(undefined, timeOptions);
-    return `${formattedDate} ${formattedTime}`; // Возвращаем дату и время
+    const formattedTime = date.toLocaleTimeString(undefined, timeOptions)
+    return `${formattedDate} ${formattedTime}` // Возвращаем дату и время
   }
 
-  return formattedDate; // Возвращаем только дату
-};
+  return formattedDate // Возвращаем только дату
+}
 
 // !usege
 // const dateString = "2024-10-10T05:36:27"; // Пример строки даты
