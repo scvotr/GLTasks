deebad34c6420909f6fe17919fb76ba519a3c3c5
Scvotr <stasforeva23@mail.ru>
2024-10-10 08:08:12 +0300

    add new function for format date time & calculate total time

--- fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorLogView/MotorRepairLogView/MotorRepairLogView.jsx
+++ fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorLogView/MotorRepairLogView/MotorRepairLogView.jsx
@@ -4,6 +4,7 @@ import { getDataFromEndpoint } from "../../../../../../../../../utils/getDataFro
 import { CustomSnackbar } from "../../../../../../../../CustomSnackbar/CustomSnackbar"
 import { Loader } from "../../../../../../../../FormComponents/Loader/Loader"
 import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Divider } from "@mui/material"
+import { formatDateV2 } from "../../../../../../../../../utils/formatDate"
 
 export const MotorRepairLogView = ({ motor }) => {
   const currentUser = useAuthContext()
@@ -46,6 +47,18 @@ export const MotorRepairLogView = ({ motor }) => {
     }
   }, [currentUser, motor])
 
+  // Функция для вычисления затраченного времени
+  const calculateTotalTime = (start, end) => {
+    console.log(start, end)
+    const startDate = new Date(start.split(" ").join("T")) // Преобразуем строку в формат ISO
+    const endDate = new Date(end.split(" ").join("T"))
+    const totalTimeInSeconds = (endDate - startDate) / 1000 // Разница в миллисекундах
+    const hours = Math.floor(totalTimeInSeconds / 3600)
+    const minutes = Math.floor((totalTimeInSeconds % 3600) / 60)
+    const seconds = Math.floor(totalTimeInSeconds % 60)
+    return `${hours}ч ${minutes}м ${seconds}с` // Форматируем результат
+  }
+
   return (
     <>
       <Loader reqStatus={response}>
@@ -57,15 +70,17 @@ export const MotorRepairLogView = ({ motor }) => {
                   <TableCell align="center">№</TableCell>
                   <TableCell align="center">Начат</TableCell>
                   <TableCell align="center">Завершён</TableCell>
+                  <TableCell align="center">Затраченное время</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {tableData &&
                   tableData.map((row, id) => (
-                    <TableRow key={row.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }} hover onClick={() => console.log('qq')}>
+                    <TableRow key={row.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }} hover onClick={() => console.log("qq")}>
                       <TableCell align="center">{id}</TableCell>
-                      <TableCell align="center">{row.repair_start_local}</TableCell>
-                      <TableCell align="center">{row.repair_end_local}</TableCell>
+                      <TableCell align="center">{formatDateV2(row.repair_start_local, true)}</TableCell>
+                      <TableCell align="center">{formatDateV2(row.repair_end_local, true)}</TableCell>
+                      <TableCell align="center">{calculateTotalTime(row.repair_start_local, row.repair_end_local)}</TableCell>
                     </TableRow>
                   ))}
               </TableBody>
