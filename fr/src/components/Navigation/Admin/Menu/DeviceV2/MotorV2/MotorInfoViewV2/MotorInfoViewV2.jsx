fff2b2a9dc6d8d6c45c983f65c60b6b197f830f8
Scvotr <stasforeva23@mail.ru>
2024-10-10 06:01:09 +0300

    add handler for use tab list

--- fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorInfoViewV2.jsx
+++ fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorInfoViewV2.jsx
@@ -2,5 +2,9 @@ import { useState } from "react"
 
 export const MotorInfoViewV2 = ({ motor }) => {
     const [value, setValue] = useState("1")
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue)
+      }
     console.log(motor)
 }
