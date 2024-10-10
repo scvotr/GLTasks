bcbb78febec29424f8ec1ec1bc76ec6561df2bf8
Scvotr <stasforeva23@mail.ru>
2024-10-10 06:10:51 +0300

    add sub tab list for logs

--- fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorInfoViewV2.jsx
+++ fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorInfoViewV2.jsx
@@ -1,14 +1,20 @@
 import TabContext from "@mui/lab/TabContext"
 import TabList from "@mui/lab/TabList"
+import TabPanel from "@mui/lab/TabPanel"
 import { Box, Tab } from "@mui/material"
 import { useState } from "react"
 
 export const MotorInfoViewV2 = ({ motor }) => {
   console.log(motor)
   const [value, setValue] = useState("1")
+  const [logTabValue, setLogTabValue] = useState("1")
 
   const handleChange = (event, newValue) => {
     setValue(newValue)
+  }
+
+  const handleLogTabChange = (event, newValue) => {
+    setLogTabValue(newValue)
   }
 
   return (
@@ -22,6 +28,13 @@ export const MotorInfoViewV2 = ({ motor }) => {
               <Tab label="Информация" value="3" />
             </TabList>
           </Box>
+          <TabPanel value="1">
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Аварии" logTabValue="1" />
+              <Tab label="Обслуживание" logTabValue="2" />
+              <Tab label="Сводная" logTabValue="3" />
+            </TabList>
+          </TabPanel>
         </TabContext>
       </Box>
     </>
