e29332c09b496bfada17cb0eb0ed21c1a038bfc1
Scvotr <stasforeva23@mail.ru>
2024-10-10 06:06:47 +0300

    add tab list for

--- fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorInfoViewV2.jsx
+++ fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorInfoViewV2.jsx
@@ -1,10 +1,29 @@
+import TabContext from "@mui/lab/TabContext"
+import TabList from "@mui/lab/TabList"
+import { Box, Tab } from "@mui/material"
 import { useState } from "react"
 
 export const MotorInfoViewV2 = ({ motor }) => {
-    const [value, setValue] = useState("1")
+  console.log(motor)
+  const [value, setValue] = useState("1")
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue)
-      }
-    console.log(motor)
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  return (
+    <>
+      <Box>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider", width: "100%", mt: 2 }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Журналы" value="1" />
+              <Tab label="Конфигурация" value="2" />
+              <Tab label="Информация" value="3" />
+            </TabList>
+          </Box>
+        </TabContext>
+      </Box>
+    </>
+  )
 }
