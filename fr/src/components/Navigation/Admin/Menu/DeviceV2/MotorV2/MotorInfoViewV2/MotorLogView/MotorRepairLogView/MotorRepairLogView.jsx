458e10882c4880c0d770a01af61123713e892831
Scvotr <stasforeva23@mail.ru>
2024-10-10 06:36:20 +0300

    add function for fetch data

--- fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorLogView/MotorRepairLogView/MotorRepairLogView.jsx
+++ fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorInfoViewV2/MotorLogView/MotorRepairLogView/MotorRepairLogView.jsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from "react"
 import { useAuthContext } from "../../../../../../../../../context/AuthProvider"
+import { getDataFromEndpoint } from "../../../../../../../../../utils/getDataFromEndpoint"
 
 export const MotorRepairLogView = ({ motor }) => {
   const currentUser = useAuthContext()
   const [response, setResponse] = useState({ loading: false, error: null })
   const [tableData, setTableData] = useState([])
+
+  const fetchData = async () => {
+    try {
+      setResponse({ loading: true, error: null })
+      const data = await getDataFromEndpoint(currentUser.token, '', "POST", motor, setResponse)
+      setTableData(...data)
+      setResponse({ loading: false, error: null })
+    } catch (error) {
+      setResponse({ loading: false, error: error.message })
+
+    }
+  }
 
 
 
