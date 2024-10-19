3ec6688b9b1976676330382f2f577cae7608ab43
scvotr <71510141+scvotr@users.noreply.github.com>
2024-10-19 08:11:47 +0300

    update trigers. now cant disable trigers. change f-key

--- bk/Database/models/Device/Motors/MotorHistory.js
+++ bk/Database/models/Device/Motors/MotorHistory.js
@@ -10,14 +10,32 @@ const createMotorsRepairHistoryTable = async (allowDrop = false) => {
         motor_id INTEGER NOT NULL,
         repair_start DATATIME,
         repair_end DATATIME,
-        FOREIGN KEY (motor_id) REFERENCES motors (id)
+        FOREIGN KEY (motor_id) REFERENCES motors (motor_id)
+        --FOREIGN KEY (motor_id) REFERENCES motors (id)
       )`
 
   await executeTableCreation('motor_repair_history', createTableQuery, allowDrop)
 }
+// Функция для сброса триггеров
+const dropRepairTriggers = async () => {
+  const dropStartTrigger = `DROP TRIGGER IF EXISTS start_repair_trigger;`
+  const dropEndTrigger = `DROP TRIGGER IF EXISTS end_repair_trigger;`
 
+  try {
+    await executeTableCreation('start_repair_trigger', dropStartTrigger)
+    await executeTableCreation('end_repair_trigger', dropEndTrigger)
+    console.log('Триггеры успешно удалены!')
+  } catch (error) {
+    console.error('Ошибка при удалении триггеров: ', error)
+    throw new Error('Не удалось удалить триггеры')
+  }
+}
 // Функция для создания триггеров
-const createRepairTriggers = async () => {
+const createRepairTriggers = async useTriggers => {
+  if (!useTriggers) {
+    console.log('Триггеры не будут созданы.')
+    return // Выход из функции, если триггеры не нужны
+  }
   const startRepairTrigger = `
     CREATE TRIGGER IF NOT EXISTS start_repair_trigger
     AFTER UPDATE OF on_repair ON motors
@@ -25,7 +43,8 @@ const createRepairTriggers = async () => {
     WHEN NEW.on_repair = TRUE AND OLD.on_repair = FALSE
     BEGIN
       INSERT INTO motor_repair_history (motor_id, repair_start)
-      VALUES (NEW.id, CURRENT_TIMESTAMP);
+      VALUES (NEW.motor_id, CURRENT_TIMESTAMP);
+      --VALUES (NEW.id, CURRENT_TIMESTAMP);
     END;
   `
 
@@ -37,7 +56,8 @@ const createRepairTriggers = async () => {
     BEGIN
       UPDATE motor_repair_history
       SET repair_end = CURRENT_TIMESTAMP
-      WHERE motor_id = NEW.id AND repair_end IS NULL;
+      WHERE motor_id = NEW.motor_id AND repair_end IS NULL;
+      --WHERE motor_id = NEW.id AND repair_end IS NULL;
     END;
   `
 
@@ -52,10 +72,16 @@ const createRepairTriggers = async () => {
   }
 }
 
-const createAllMotorHistory = async (allowDrop = false) => {
+const createAllMotorHistory = async (allowDrop = false, useTriggers = false) => {
   try {
-    await createMotorsRepairHistoryTable(allowDrop)
-    await createRepairTriggers() // Создание триггеров после создания таблицы
+    await createMotorsRepairHistoryTable(true)
+    // await createRepairTriggers() // Создание триггеров после создания таблицы
+    // Если useTriggers равно false, удаляем триггеры
+    if (!useTriggers) {
+      await dropRepairTriggers()
+    } else {
+      await createRepairTriggers(useTriggers) // Создание триггеров
+    }
   } catch (error) {
     console.log('Error creating motor history tables: ', error)
     throw new Error('Failed to create all motor history tables')
