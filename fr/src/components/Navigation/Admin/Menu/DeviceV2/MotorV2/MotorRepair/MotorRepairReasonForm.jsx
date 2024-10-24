b980ceae16c261597dc0064f1dbc4d697685f6ef
Scvotr <stasforeva23@mail.ru>
2024-10-24 08:05:05 +0300

    add fetch to submit handler

--- fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorRepair/MotorRepairReasonForm.jsx
+++ fr/src/components/Navigation/Admin/Menu/DeviceV2/MotorV2/MotorRepair/MotorRepairReasonForm.jsx
@@ -1,5 +1,7 @@
-import { Box, TextField, Button, Select, MenuItem, FormControl, InputLabel, Stack } from "@mui/material";
-import { useState, useEffect } from "react";
+import { Box, TextField, Button, Select, MenuItem, FormControl, InputLabel, Stack } from "@mui/material"
+import { useState, useEffect } from "react"
+import { getDataFromEndpoint } from "../../../../../../../utils/getDataFromEndpoint"
+import { useAuthContext } from "../../../../../../../context/AuthProvider"
 
 // Пример массива техников
 const technicians = [
@@ -7,45 +9,56 @@ const technicians = [
   { id: 2, name: "Петр Петров" },
   { id: 3, name: "Сидор Сидоров" },
   { id: 4, name: "Анна Аннова" },
-];
+]
 
-export const MotorRepairReasonForm = ({ motor, onClose }) => {
-  console.log('MotorRepairReasonForm', motor);
+export const MotorRepairReasonForm = ({ motor, popupSnackbar, onClose }) => {
+  console.log("MotorRepairReasonForm", motor)
+  const currentUser = useAuthContext()
+  const [reqStatus, setReqStatus] = useState({ loading: false, error: null })
 
   const today = new Date().toISOString().split("T")[0]
 
   const [formData, setFormData] = useState({
-    repairReason: '',
-    technicianId: '',
-    additionalNotes: '',
-    repairEndTime: ''
-  });
+    repairReason: "",
+    technicianId: "",
+    additionalNotes: "",
+    repairEndTime: "",
+  })
 
   // Устанавливаем минимальное значение для времени окончания ремонта
-  const [minDateTime, setMinDateTime] = useState('');
+  const [minDateTime, setMinDateTime] = useState("")
 
   useEffect(() => {
-    const now = new Date();
-    const formattedDate = now.toISOString().slice(0, 16); // Форматируем дату в 'YYYY-MM-DDTHH:mm'
-    setMinDateTime(formattedDate);
-  }, []);
+    const now = new Date()
+    const formattedDate = now.toISOString().slice(0, 16) // Форматируем дату в 'YYYY-MM-DDTHH:mm'
+    setMinDateTime(formattedDate)
+  }, [])
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = e => {
+    const { name, value } = e.target
     setFormData({
       ...formData,
-      [name]: value
-    });
-  };
+      [name]: value,
+    })
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Отправка данных:', formData);
+  const handleSubmit = async e => {
+    e.preventDefault()
+    console.log("Отправка данных:", formData)
+    try {
+      setReqStatus({ loading: true, error: null })
+      const res = await getDataFromEndpoint(currentUser.token, `/admin/devices/motor/takeMotorForRepair`, "POST", motor.motor_id, setReqStatus)
+      popupSnackbar(res)
+      setReqStatus({ loading: false, error: null })
+    } catch (error) {
+      setReqStatus({ loading: false, error: error.message })
+      popupSnackbar(`Ошибка: ${error.message} Код: ${error.code}`, "error")
+    }
     onClose()
-  };
+  }
 
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', gap: 2, m: 10, pr: 20, pl:20 }}>
+    <Box component="form" onSubmit={handleSubmit} sx={{ display: "flex", flexDirection: "column", gap: 2, m: 10, pr: 20, pl: 20 }}>
       <Stack direction="row" spacing={2}>
         <FormControl fullWidth required sx={{ flex: 1 }}>
           <InputLabel id="repair-reason-label">Причина ремонта</InputLabel>
@@ -55,8 +68,7 @@ export const MotorRepairReasonForm = ({ motor, onClose }) => {
             label="Причина ремонта"
             name="repairReason"
             value={formData.repairReason}
-            onChange={handleChange}
-          >
+            onChange={handleChange}>
             <MenuItem value="неисправность">Неисправность</MenuItem>
             <MenuItem value="техническое обслуживание">Техническое обслуживание</MenuItem>
             <MenuItem value="модернизация">Модернизация</MenuItem>
@@ -66,15 +78,8 @@ export const MotorRepairReasonForm = ({ motor, onClose }) => {
 
         <FormControl fullWidth required sx={{ flex: 1 }}>
           <InputLabel id="technician-label">Исполнитель</InputLabel>
-          <Select
-            labelId="technician-label"
-            id="technician"
-            label="Исполнитель"
-            name="technicianId"
-            value={formData.technicianId}
-            onChange={handleChange}
-          >
-            {technicians.map((technician) => (
+          <Select labelId="technician-label" id="technician" label="Исполнитель" name="technicianId" value={formData.technicianId} onChange={handleChange}>
+            {technicians.map(technician => (
               <MenuItem key={technician.id} value={technician.id}>
                 {technician.name}
               </MenuItem>
@@ -107,11 +112,11 @@ export const MotorRepairReasonForm = ({ motor, onClose }) => {
         placeholder="Введите дополнительные заметки (обязательно)"
         required
       />
-      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
         <Button type="submit" variant="contained" color="primary">
           Забрать на ремонт
         </Button>
       </Box>
     </Box>
-  );
-};
+  )
+}
