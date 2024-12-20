04ac24fa6ae7174220cb57a3bb23b46acd8e53cd
Scvotr <stasforeva23@mail.ru>
2024-12-20 15:58:10 +0300

    add router handler for lab

---
+++ bk/routingHandlers/handelLabRoutes.js
@@ -0,0 +1,40 @@
+const { handleDefaultRoute } = require("./handleDefaultRoute");
+const { protectRouteTkPl } = require("../utils/protectRouteTkPl");
+const LabController = require("../controllers/Lab/LabController");
+
+
+const routeHandlers = {
+  "/lab/createReqForAvailability": LabController.addNewLabsReqForAvailability,
+  "/lab/getAllRequestsWithApprovals": LabController.getAllRequestsWithApprovals,
+  "/lab/getUserConfirmation": LabController.getUserConfirmation,
+  "/lab/updateReadStatus": LabController.updateReadStatus,
+}
+
+const handelLabRoutes = async(req, res) => {
+  const { url, method } = req;
+  try {
+    if (url.startsWith("/lab")) {
+      if (method === "POST") {
+        const routeHandler = routeHandlers[url];
+        if (routeHandler) {
+          await protectRouteTkPl(routeHandler)(req, res);
+        } else {
+          handleDefaultRoute(req, res);
+        }
+      } else {
+        handleDefaultRoute(req, res);
+      }
+    } else {
+      handleDefaultRoute(req, res);
+    }
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "handelLabRoutes - ERROR" }));
+  }
+}
+
+module.exports = {
+  handelLabRoutes
+};
