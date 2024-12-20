b0e4980e9a76ef840d19628a666361fc0aa0840b
Scvotr <stasforeva23@mail.ru>
2024-12-20 16:02:48 +0300

    wtf sales or energy!?!!?

---
+++ fr/src/components/Navigation/User/Menu/Users/SalesDepartment/Chife/getAllSalesChife.jsx
@@ -0,0 +1,32 @@
+import BiotechOutlinedIcon from '@mui/icons-material/BiotechOutlined';
+import ProductionQuantityLimitsOutlinedIcon from '@mui/icons-material/ProductionQuantityLimitsOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2';
+
+
+export const getAllSalesChife = (currentUser) => {
+  const energyServicePositions = [2, 33, 15,  16, 27, 28 ,45, 47, ] // Массив с допустимыми позициями получить всех энергетиков с сервера!
+
+  if (energyServicePositions.includes(Number(currentUser.position))) {
+    return {
+      name: "Лаборатория",
+      icon: <BiotechOutlinedIcon fontSize="large" />,
+      path: "/labForSales",
+      tasksCount: 0,
+      subItems: [
+        {
+          name: "Запрос на наличие",
+          icon: <ProductionQuantityLimitsOutlinedIcon fontSize="large" />,
+          path: "/labForSales/requestForAvailability",
+          btn: true,
+        },
+        {
+          name: "Архив",
+          icon: <Inventory2OutlinedIcon fontSize="large" />,
+          path: "/labForSales/archive",
+          btn: true,
+        },
+      ],
+    }
+  }
+  return null // Возвращаем null, если условие не выполнено
+}
