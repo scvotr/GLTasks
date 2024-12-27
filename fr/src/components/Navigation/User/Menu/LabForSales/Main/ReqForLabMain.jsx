f7e1245af44b6df150c288926940827faf84ed28
Scvotr <stasforeva23@mail.ru>
2024-12-27 07:42:38 +0300

    add reset handlers in props

--- fr/src/components/Navigation/User/Menu/LabForSales/Main/ReqForLabMain.jsx
+++ fr/src/components/Navigation/User/Menu/LabForSales/Main/ReqForLabMain.jsx
@@ -4,13 +4,23 @@ import { OwnersTabs } from "./Tabs/OwnersTabs"
 const ELEVATOR_AE = "3"
 const ELEVATOR_PE = "4"
 
-export const ReqForLabMain = ({ requests = [], currentUser, reRender, addNewRequest, approvedRequest }) => {
+export const ReqForLabMain = ({ requests = [], currentUser, reRender, addNewRequest, approvedRequest, resetApprovedRequest, resetAddNewRequest }) => {
   const isElevator = currentUser.dep.toString() === ELEVATOR_AE || currentUser.dep.toString() === ELEVATOR_PE
 
   return (
     <>
-      {isElevator && <ElevatorTabs requests={requests} currentUser={currentUser} reRender={reRender} />}
-      {!isElevator && <OwnersTabs requests={requests} currentUser={currentUser} reRender={reRender} addNewRequest={addNewRequest} approvedRequest={approvedRequest}/>}
+      {isElevator && <ElevatorTabs requests={requests} currentUser={currentUser} reRender={reRender} resetApprovedRequest={resetApprovedRequest} resetAddNewRequest={resetAddNewRequest}/>}
+      {!isElevator && (
+        <OwnersTabs
+          requests={requests}
+          currentUser={currentUser}
+          reRender={reRender}
+          addNewRequest={addNewRequest}
+          approvedRequest={approvedRequest}
+          resetApprovedRequest={resetApprovedRequest}
+          resetAddNewRequest={resetAddNewRequest}
+        />
+      )}
     </>
   )
 }
