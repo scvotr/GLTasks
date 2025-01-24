b69242694406fc9a07b00ed0a97d79e59bbde92d
Scvotr <stasforeva23@mail.ru>
2025-01-24 06:35:46 +0300

    add function for notice then new comments to labReq

--- bk/controllers/Lab/LabNoticeFor.js
+++ bk/controllers/Lab/LabNoticeFor.js
@@ -26,7 +26,33 @@ const noticeForLabSystemUsersT = async (user_id, text) => {
     throw new Error('Ошибка при отправке уведомления пользователю')
   }
 }
+// !----------------------------------------------------
+const noticeForLabSystemUsersTNewCommentT = async (user_id, text) => {
+  const io = socketManager.getIO()
+
+  // Отправка уведомления через сокеты
+  io.to('user_' + user_id).emit('reqForLabNewComment', { message: text, taskData: user_id })
+
+  try {
+    // Отправка email пользователю
+    //! can remove await
+    //! await sendEmailToUser(user_id, text)
+    // Отправка email пользователю без ожидания 
+    sendEmailToUser(user_id, text)
+      .then(() => {
+        // console.log('Email успешно отправлен пользователю', user_id)
+      })
+      .catch(error => {
+        // console.error('Ошибка при отправке email:', error)
+        // Дополнительные действия по обработке ошибки, если необходимо
+      })
+  } catch (error) {
+    console.error('Ошибка при отправке email:', error)
+    throw new Error('Ошибка при отправке уведомления пользователю')
+  }
+}
 
 module.exports = {
   noticeForLabSystemUsersT,
+  noticeForLabSystemUsersTNewCommentT,
 }
