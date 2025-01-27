2dca9b7acf06a9bbf29d9ae2c0842098c03cd4de
Scvotr <stasforeva23@mail.ru>
2025-01-27 15:11:47 +0300

    remove unusege comments

--- bk/controllers/Tasks/noticeFor.js
+++ bk/controllers/Tasks/noticeFor.js
@@ -5,19 +5,35 @@ const {
 } = require("../../utils/socket/socketManager");
 const { sendEmailToUser, sendEmailToLead, sendEmailToGeneral } = require("./mailFor");
 
+// const noticeToAppointUserT = async (text, data) => {
+//   const io = socketManager.getIO()
+//   io.to('user_' + data.appoint_user_id)
+//     .emit('taskApproved', { message: text, taskData: data.task_id });
+//     try {
+//       await updateReadStatusQ({ task_id: data.task_id, user_id: data.appoint_user_id, read_status: 'unread' });
+//       await sendEmailToUser(data.appoint_user_id, text, data)
+//     } catch (error) {
+//       throw new Error('Ошибка запроса к базе данных', error)
+//     }
+// }
+
 const noticeToAppointUserT = async (text, data) => {
-  const io = socketManager.getIO()
-  io.to('user_' + data.appoint_user_id)
-    .emit('taskApproved', { message: text, taskData: data.task_id });
-    try {
-      await updateReadStatusQ({ task_id: data.task_id, user_id: data.appoint_user_id, read_status: 'unread' });
-      //! can remove await
-      await sendEmailToUser(data.appoint_user_id, text, data)
-      // sendEmailToUser(data.appoint_user_id, text, data)
-    } catch (error) {
-      throw new Error('Ошибка запроса к базе данных', error)
-    }
-};
+  const io = socketManager.getIO();
+  
+  if (io.sockets.sockets.has('user_' + data.appoint_user_id)) {
+    io.to('user_' + data.appoint_user_id)
+      .emit('taskApproved', { message: text, taskData: data.task_id });
+  } else {
+    console.warn(`Пользователь ${data.appoint_user_id} не подключен`);
+  }
+
+  try {
+    await updateReadStatusQ({ task_id: data.task_id, user_id: data.appoint_user_id, read_status: 'unread' });
+    await sendEmailToUser(data.appoint_user_id, text, data);
+  } catch (error) {
+    console.error('Ошибка при обновлении статуса или отправке email', error);
+  }
+}
 
 const noticeToAppointLeadT = async (text, data) => {
   const io = socketManager.getIO()
@@ -27,52 +43,47 @@ const noticeToAppointLeadT = async (text, data) => {
       await updateReadStatusQ({ task_id: data.task_id, user_id: data.appoint_subdepartment_id, read_status: 'unread' });
       // !----------------
       await updateReadStatusQ({ task_id: data.task_id, user_id: data.responsible_subdepartment_id, read_status: 'readed' });
-      //! can remove await
       await sendEmailToLead(data.appoint_subdepartment_id, text, data)
-      // sendEmailToLead(data.appoint_subdepartment_id, text, data)
     } catch (error) {
       throw new Error('Ошибка запроса к базе данных', error)
     }
-};
+}
+
 const noticeToGeneralT = async (text, data) => {
   const io = socketManager.getIO()
   io.to('generalDep_' + data.appoint_department_id)
     .emit('taskApproved', { message: text, taskData: data.task_id });
     try {
       await updateReadStatusQ({ task_id: data.task_id, user_id: data.appoint_department_id, read_status: 'unread' });
-      //! can remove await
       await sendEmailToGeneral(data.appoint_department_id, text, data)
-      // sendEmailToGeneral(data.appoint_department_id, text, data)
     } catch (error) {
-      throw new Error('Ошибка запроса к базе данных', error)
+      // throw new Error('Ошибка запроса к базе данных', error)
     }
-};
+}
+
 const noticeAppoinToGeneralT = async (text, data) => {
   const io = socketManager.getIO()
   io.to('generalDep_' + data.appoint_department_id)
     .emit('taskApproved', { message: text, taskData: data.task_id });
     try {
       await updateReadStatusQ({ task_id: data.task_id, user_id: data.appoint_department_id, read_status: 'unread' });
-      //! can remove await
       await sendEmailToGeneral(data.appoint_department_id, text, data)
-      // sendEmailToGeneral(data.appoint_department_id, text, data)
     } catch (error) {
       throw new Error('Ошибка запроса к базе данных', error)
     }
-};
+}
+
 const noticeResponceToGeneralT = async (text, data) => {
   const io = socketManager.getIO()
   io.to('generalDep_' + data.responsible_department_id)
     .emit('taskApproved', { message: text, taskData: data.task_id });
     try {
       await updateReadStatusQ({ task_id: data.task_id, user_id: data.responsible_department_id, read_status: 'unread' });
-      //! can remove await
       await sendEmailToGeneral(data.responsible_department_id, text, data)
-      // sendEmailToGeneral(data.responsible_department_id, text, data)
     } catch (error) {
       throw new Error('Ошибка запроса к базе данных', error)
     }
-};
+}
 
 const noticeToResponceUserT = async (text, data) => {
   const io = socketManager.getIO()
@@ -80,13 +91,11 @@ const noticeToResponceUserT = async (text, data) => {
     .emit('taskApproved', { message: text, taskData: data.task_id });
     try {
       await updateReadStatusQ({ task_id: data.task_id, user_id: data.responsible_user_id, read_status: 'unread' });
-      //! can remove await
       await sendEmailToUser(data.responsible_user_id, text, data)
-      // sendEmailToUser(data.responsible_user_id, text, data)
     } catch (error) {
       throw new Error('Ошибка запроса к базе данных', error)
     }
-};
+}
 
 const noticeToResponceLeadT = async (text, data) => {
   const io = socketManager.getIO()
@@ -96,13 +105,11 @@ const noticeToResponceLeadT = async (text, data) => {
       await updateReadStatusQ({ task_id: data.task_id, user_id: data.responsible_subdepartment_id, read_status: 'unread' });
       // !----------------
       await updateReadStatusQ({ task_id: data.task_id, user_id: data.appoint_subdepartment_id, read_status: 'readed' });
-      //! can remove await
       await sendEmailToLead(data.responsible_subdepartment_id, text, data)
-      // sendEmailToLead(data.responsible_subdepartment_id, text, data)
     } catch (error) {
       throw new Error('Ошибка запроса к базе данных', error)
     }
-};
+}
 
 module.exports ={
   noticeToAppointUserT,
