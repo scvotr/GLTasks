fcaae12ce47ce1798bc3a2f1182c7f1811a512d6
Scvotr <stasforeva23@mail.ru>
2025-01-27 15:08:57 +0300

    update cmp

--- bk/socketService.js
+++ bk/socketService.js
@@ -1,111 +1,109 @@
 'use strict'
 
 const jwt = require('jsonwebtoken')
-require('dotenv').config();
-const logger = require('./utils/logger/logger');
+require('dotenv').config()
+const logger = require('./utils/logger/logger')
 
-let userSocketPool = [];
+let userSocketPool = []
 
 function addUserInCache(userId) {
-  console.log('addUserInSocketCache: ', userId)
+  console.log('Добавление пользователя в кэш: ', userId)
   if (!userSocketPool.includes(userId)) {
     //! if(!userSocketPool.some(id => id === userId)) {
     userSocketPool.push(userId)
-    console.log('Активных пользователй: ', userSocketPool)
+    console.log('Активных пользователей: ', userSocketPool)
   }
 }
 
 function removeUserFromCache(userId) {
-  console.log('removeUserFromSocketCache', userId)
-  userSocketPool = userSocketPool.filter(id => id !== userId);
-  console.log('Активных пользователй: ', userSocketPool)
+  console.log('Удаление пользователя из кэша: ', userId)
+  userSocketPool = userSocketPool.filter(id => id !== userId)
+  console.log('Активных пользователей: ', userSocketPool)
 }
 
 function authenticateUserSocket(socket, next) {
-  let token = socket.handshake.query.token;
-  let tokenInHeaders = socket.handshake.headers.authorization;
+  let token = socket.handshake.query.token
+  let tokenInHeaders = socket.handshake.headers.authorization
   if (tokenInHeaders) {
-    tokenInHeaders = tokenInHeaders.slice(7, tokenInHeaders.length);
+    tokenInHeaders = tokenInHeaders.slice(7, tokenInHeaders.length)
     jwt.verify(tokenInHeaders, process.env.KEY_TOKEN, (err, decoded) => {
       if (err) {
         logger.errorAuth({
           message: 'Authentication failed: invalid token',
-          token: tokenInHeaders
-        });
-        return next(new Error('Authentication error'));
+          token: tokenInHeaders,
+        })
+        return next(new Error('Authentication error'))
       }
-      socket.decoded = decoded;
+      socket.decoded = decoded
       logger.infoAuth({
         message: 'Authentication successful',
-        decode: socket.decoded.name
-      });
-      return next();
-    });
+        decode: socket.decoded.name,
+      })
+      return next()
+    })
   } else {
     logger.warn({
-      message: 'Authentication failed: token is missing'
-    });
-    return next(new Error('Authentication error'));
+      message: 'Authentication failed: token is missing',
+    })
+    return next(new Error('Authentication error'))
   }
 }
 
 function setupSocket(io) {
-  io.use(authenticateUserSocket)
-    .on('connection', (socket) => {
-      addUserInCache(socket.decoded.id)
+  io.use(authenticateUserSocket).on('connection', socket => {
+    addUserInCache(socket.decoded.id)
 
-      socket.join('allActiveUser')
-      socket.join('user_' + socket.decoded.id) //?для каждого пользователя
-      socket.join('dep_' + socket.decoded.department_id) //? для каждого подразделения
-      socket.join('subDep_' + socket.decoded.subdepartment_id) //? для каждого отдела
+    socket.join('allActiveUser')
+    socket.join('user_' + socket.decoded.id) //?для каждого пользователя
+    socket.join('dep_' + socket.decoded.department_id) //? для каждого подразделения
+    socket.join('subDep_' + socket.decoded.subdepartment_id) //? для каждого отдела
 
-      if (socket.decoded.role === 'general') {
-        let generalRoomName = 'generalDep_' + socket.decoded.department_id
-        socket.join(generalRoomName)
+    if (socket.decoded.role === 'general') {
+      let generalRoomName = 'generalDep_' + socket.decoded.department_id
+      socket.join(generalRoomName)
 
-        socket.on('newMessage', (message) => {
-          // Это пример, внутренняя логика зависит от вашего предназначения
-          io.to(generalRoomName).emit('messageReceived', message);
-        });
-      }
-      if (socket.decoded.role === 'chife') {
-        let leadRoomName = 'leadSubDep_' + socket.decoded.subdepartment_id
-        socket.join(leadRoomName)
-
-        socket.on('newMessage', (message) => {
-          // Это пример, внутренняя логика зависит от вашего предназначения
-          io.to(leadRoomName).emit('messageReceived', message);
-        });
-      }
-      if(socket.decoded.role === 'admin') {
-        let adminRoomName = 'admin_' + socket.decoded.id
-        socket.join(adminRoomName)
-
-        socket.on('newMessage', (message) => {
-          io.to(adminRoomName).emit('messageReceived', message);
-        });
-      }
-
-      socket.on('getMyRooms', () => {
-        const allRooms = Array.from(socket.rooms)
-        socket.emit('yourRooms',
-          allRooms.filter((room) => room !== socket.id)
-        )
+      socket.on('newMessage', message => {
+        // Это пример, внутренняя логика зависит от вашего предназначения
+        io.to(generalRoomName).emit('messageReceived', message)
       })
+    }
+    if (socket.decoded.role === 'chife') {
+      let leadRoomName = 'leadSubDep_' + socket.decoded.subdepartment_id
+      socket.join(leadRoomName)
 
-      socket.on('disconnect', () => {
-        removeUserFromCache(socket.decoded.id)
+      socket.on('newMessage', message => {
+        // Это пример, внутренняя логика зависит от вашего предназначения
+        io.to(leadRoomName).emit('messageReceived', message)
       })
+    }
+    if (socket.decoded.role === 'admin') {
+      let adminRoomName = 'admin_' + socket.decoded.id
+      socket.join(adminRoomName)
 
-      socket.on('error', (error) => {
-        console.error('Произошла ошибка сокета для пользователя с ID:', socket.decoded.id, error)
-        logger.error('Произошла ошибка сокета для пользователя с ID:', socket.decoded.id, error)
-      });
+      socket.on('newMessage', message => {
+        io.to(adminRoomName).emit('messageReceived', message)
+      })
+    }
+
+    socket.on('getMyRooms', () => {
+      const allRooms = Array.from(socket.rooms)
+      socket.emit(
+        'yourRooms',
+        allRooms.filter(room => room !== socket.id)
+      )
     })
+
+    socket.on('disconnect', () => {
+      removeUserFromCache(socket.decoded.id)
+    })
+
+    socket.on('error', error => {
+      console.error('Произошла ошибка сокета для пользователя с ID:', socket.decoded.id, error)
+      logger.error('Произошла ошибка сокета для пользователя с ID:', socket.decoded.id, error)
+    })
+  })
 }
 
 module.exports = {
   setupSocket,
 }
-
-
