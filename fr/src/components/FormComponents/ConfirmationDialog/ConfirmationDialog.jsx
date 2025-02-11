38f8ef689ecaec2b23426f6363fa06cd8719c03b
Scvotr <stasforeva23@mail.ru>
2025-02-11 09:55:41 +0300

    change button place

--- fr/src/components/FormComponents/ConfirmationDialog/ConfirmationDialog.jsx
+++ fr/src/components/FormComponents/ConfirmationDialog/ConfirmationDialog.jsx
@@ -21,11 +21,11 @@ export const ConfirmationDialog = ({ open, onClose, onConfirm, title, message })
           <DialogContentText>{message}</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={onClose} variant="contained">
-            Отмена
-          </Button>
           <Button onClick={handleConfirmDelete} variant="contained" color="error">
             Подтвердить
+          </Button>
+          <Button onClick={onClose} variant="contained">
+            Отмена
           </Button>
         </DialogActions>
       </Box>
