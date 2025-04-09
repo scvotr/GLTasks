bea9859ed26ed6bda591d6dd1f18c90757f7d249
Scvotr <stasforeva23@mail.ru>
2025-04-09 11:23:45 +0300

    added an array of job IDs for route access

--- fr/src/routes/RoutesList/LabForSales/LabForSalesRoutesList.js
+++ fr/src/routes/RoutesList/LabForSales/LabForSalesRoutesList.js
@@ -1,13 +1,12 @@
-import { LabForSalesMain } from "../../../components/Navigation/User/Menu/LabForSales/LabForSalesMain";
-import { LabRequestForAvailability } from "../../../components/Navigation/User/Menu/LabForSales/LabRequestForAvailability";
-import { MutualVerificationArchive } from "../../../components/Navigation/User/Menu/MutualVerification/MutualVerificationArchive";
-import { MutualVerificationDocs } from "../../../components/Navigation/User/Menu/MutualVerification/MutualVerificationDocs";
-import { PrivateRoutesCheck } from "../../PrivateRoutesCheck";
+import { LabForSalesMain } from '../../../components/Navigation/User/Menu/LabForSales/LabForSalesMain'
+import { LabRequestForAvailability } from '../../../components/Navigation/User/Menu/LabForSales/LabRequestForAvailability'
+import { MutualVerificationArchive } from '../../../components/Navigation/User/Menu/MutualVerification/MutualVerificationArchive'
+import { PrivateRoutesCheck } from '../../PrivateRoutesCheck'
 
 export const LabForSalesRoutesList = [
   {
     path: '/labForSales',
-    element: <PrivateRoutesCheck component={LabForSalesMain} roles={['chife', 'user', 'general']} />,
+    element: <PrivateRoutesCheck component={LabForSalesMain} roles={['chife', 'user', 'general']} position={[4, 2, 33, 47, 15, 16, 27, 28]}/>,
   },
   {
     path: '/labForSales/requestForAvailability',
