7c7a0b41ee5f8ba52ff7cc57b959f3ef2ece1bc5
Scvotr <stasforeva23@mail.ru>
2025-06-10 10:27:45 +0300

    add route for cmp to view closed requests

--- fr/src/routes/RoutesList/LabForSales/LabForSalesRoutesList.js
+++ fr/src/routes/RoutesList/LabForSales/LabForSalesRoutesList.js
@@ -1,6 +1,6 @@
 import { LabForSalesMain } from '../../../components/Navigation/User/Menu/LabForSales/LabForSalesMain'
 import { LabRequestForAvailability } from '../../../components/Navigation/User/Menu/LabForSales/LabRequestForAvailability'
-import { MutualVerificationArchive } from '../../../components/Navigation/User/Menu/MutualVerification/MutualVerificationArchive'
+import { ReqLabForSalesClosed } from '../../../components/Navigation/User/Menu/LabForSales/ReqLabForSalesClosed'
 import { PrivateRoutesCheck } from '../../PrivateRoutesCheck'
 
 export const LabForSalesRoutesList = [
@@ -9,11 +9,11 @@ export const LabForSalesRoutesList = [
     element: <PrivateRoutesCheck component={LabForSalesMain} roles={['chife', 'user', 'general']} position={[4, 2, 33, 47, 15, 16, 27, 28]}/>,
   },
   {
-    path: '/labForSales/requestForAvailability',
-    element: <PrivateRoutesCheck component={LabRequestForAvailability} roles={['chife', 'user', 'general']} />,
+    path: '/labForSales/closed',
+    element: <PrivateRoutesCheck component={ReqLabForSalesClosed} roles={['chife', 'user', 'general']} position={[4, 2, 33, 47, 15, 16, 27, 28]}/>,
   },
   {
-    path: '/labForSales/archive',
-    element: <PrivateRoutesCheck component={MutualVerificationArchive} roles={['chife', 'user', 'general']} />,
+    path: '/labForSales/requestForAvailability',
+    element: <PrivateRoutesCheck component={LabRequestForAvailability} roles={['chife', 'user', 'general']} position={[4, 2, 33, 47, 15, 16, 27, 28]}/>,
   },
 ]
