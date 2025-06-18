5892c1126c6a450f98aa5df15edc4fc1b02c582a
Scvotr <stasforeva23@mail.ru>
2025-06-18 08:34:54 +0300

    rm filter for closed request

--- fr/src/components/Navigation/User/Menu/LabForSales/Tables/v2/ReqForLabTableView.jsx
+++ fr/src/components/Navigation/User/Menu/LabForSales/Tables/v2/ReqForLabTableView.jsx
@@ -1,27 +1,10 @@
-import { useEffect, useState } from "react"
 import { ReqForLabTable } from "./Tables/ReqForLabTable"
 
 export const ReqForLabTableView = ({ requests, currentUser, reRender, checkFullScreenOpen, setCheckFullScreenOpen, isClosedView }) => {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    if (currentUser.login) {
-      if (!Array.isArray(requests)) return
-      const filteredData = requests.filter(req => {
-        // Если запрос НЕ закрыт — оставляем его
-        if (req.req_status !== "closed") return true
-        // Если запрос закрыт, проверяем, не прочитан ли он текущим пользователем
-        const isReadByUser = req.users?.some(user => user.user_id === currentUser.id && user.read_status === "readed")
-        // Оставляем запрос, только если он НЕ прочитан пользователем
-        return !isReadByUser
-      })
-      setData(filteredData)
-    }
-  }, [requests, currentUser])
-
   return (
     <>
       <ReqForLabTable
-        requests={isClosedView ? requests : data}
+        requests={requests}
         currentUser={currentUser}
         reRender={reRender}
         setCheckFullScreenOpen={setCheckFullScreenOpen}
